db3af51c4bf357e6c26a30bf4e2aab9d2d03c44c
bblurock <llwbenson@gmail.com>
2014-04-15 16:10:58 +0800

    Modified members model query method.

--- administrator/components/com_schedule/model/members.php
+++ administrator/components/com_schedule/model/members.php
@@ -71,11 +71,7 @@ class ScheduleModelMembers extends ListModel
 	{
 		$queryHelper = $this->getContainer()->get('model.members.helper.query', Container::FORCE_NEW);
 
-		$queryHelper->addTable('member', '#__schedule_members')
-			->addTable('category',  '#__categories', 'member.catid      = category.id')
-			->addTable('user',      '#__users',      'member.created_by = user.id')
-			->addTable('viewlevel', '#__viewlevels', 'member.access     = viewlevel.id')
-			->addTable('lang',      '#__languages',  'member.language   = lang.lang_code');
+		$queryHelper->addTable('member', '#__schedule_members');
 
 		$this->filterFields = array_merge($this->filterFields, $queryHelper->getFilterFields());
 	}
