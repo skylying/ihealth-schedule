00304714cd1fa9a820acdf522d2f19500d78a656
bblurock <llwbenson@gmail.com>
2014-04-16 17:08:38 +0800

    Update tasks list query method.

--- administrator/components/com_schedule/model/tasks.php
+++ administrator/components/com_schedule/model/tasks.php
@@ -71,11 +71,7 @@ class ScheduleModelTasks extends ListModel
 	{
 		$queryHelper = $this->getContainer()->get('model.tasks.helper.query', Container::FORCE_NEW);
 
-		$queryHelper->addTable('task', '#__schedule_tasks')
-			->addTable('category',  '#__categories', 'task.catid      = category.id')
-			->addTable('user',      '#__users',      'task.created_by = user.id')
-			->addTable('viewlevel', '#__viewlevels', 'task.access     = viewlevel.id')
-			->addTable('lang',      '#__languages',  'task.language   = lang.lang_code');
+		$queryHelper->addTable('task', '#__schedule_tasks');
 
 		$this->filterFields = array_merge($this->filterFields, $queryHelper->getFilterFields());
 	}
