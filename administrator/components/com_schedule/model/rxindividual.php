17277b53a820cdfae0d981923a12f93e687902e0
Lewis Jang <Lewis.AEdes@gmail.com>
2014-04-17 10:53:18 +0800

    add model RX individual, resident

---
+++ administrator/components/com_schedule/model/rxindividual.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Windwalker\Model\AdminModel;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelRxindividual
+ *
+ * @since 1.0
+ */
+class ScheduleModelRxindividual extends AdminModel
+{
+	/**
+	 * Property prefix.
+	 *
+	 * @var  string
+	 */
+	protected $prefix = 'schedule';
+
+	/**
+	 * Property option.
+	 *
+	 * @var  string
+	 */
+	protected $option = 'com_schedule';
+
+	/**
+	 * Property textPrefix.
+	 *
+	 * @var string
+	 */
+	protected $textPrefix = 'COM_SCHEDULE';
+
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'rxindividual';
+
+	/**
+	 * Property viewItem.
+	 *
+	 * @var  string
+	 */
+	protected $viewItem = 'rxindividual';
+
+	/**
+	 * Property viewList.
+	 *
+	 * @var  string
+	 */
+	protected $viewList = 'rxindividuals';
+
+	/**
+	 * Method to set new item ordering as first or last.
+	 *
+	 * @param   JTable $table    Item table to save.
+	 * @param   string $position 'first' or other are last.
+	 *
+	 * @return  void
+	 */
+	public function setOrderPosition($table, $position = 'last')
+	{
+		parent::setOrderPosition($table, $position);
+	}
+}
