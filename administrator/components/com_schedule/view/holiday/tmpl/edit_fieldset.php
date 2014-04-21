e91e2fbd203409d2562ca816b2a4a4d87d613858
skylying <linsutim@gmail.com>
2014-04-21 14:18:58 +0800

    remove unnecessary tabs of holiday view

--- administrator/components/com_schedule/view/holiday/tmpl/edit_fieldset.php
+++ administrator/components/com_schedule/view/holiday/tmpl/edit_fieldset.php
@@ -12,10 +12,6 @@ defined('_JEXEC') or die;
 $fieldset = $data->fieldset;
 ?>
 <fieldset id="holiday-edit-fieldset-<?php echo $fieldset->name ?>" class="<?php echo $data->class ?>">
-	<legend>
-		<?php echo $fieldset->label ? JText::_($fieldset->label) : JText::_('COM_SCHEDULE_EDIT_FIELDSET_' . $fieldset->name); ?>
-	</legend>
-
 	<?php foreach ($data->form->getFieldset($fieldset->name) as $field): ?>
 		<div id="control_<?php echo $field->id; ?>">
 			<?php echo $field->getControlGroup() . "\n\n"; ?>
