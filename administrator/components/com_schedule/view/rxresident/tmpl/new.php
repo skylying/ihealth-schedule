faab17b537e346f23129464eeb1af4e82a529b01
Lewis Jang <Lewis.AEdes@gmail.com>
2014-04-21 14:53:24 +0800

    rx resident form

---
+++ administrator/components/com_schedule/view/rxresident/tmpl/new.php
@@ -0,0 +1,117 @@
+<table class="table">
+	<thead>
+	<tr>
+		<th style="width:  3%;">編號</th>
+		<th style="width:  8%;">住民</th>
+		<th style="width:  8%;">身分證字號</th>
+		<th style="width: 10%;">生日</th>
+		<th style="width: 10%;">就醫日期</th>
+		<th style="width:  5%;">處方箋天數</th>
+		<th style="width:  5%;">可調劑次數</th>
+		<th style="width: 10%;">處方箋外送次數</th>
+		<th style="width:  8%;">藥吃完日</th>
+		<th style="width: 10%;">處方箋上傳(可復選)</th>
+		<th style="width:  8%;">處方箋取得方式</th>
+		<th style="width: 10%;">複製/刪除</th>
+		<th style="width:  4%;">備註</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<!-- id -->
+		<td>
+
+		</td>
+
+		<!-- 住民 -->
+		<td>
+			<input name="" class="span12" type="text" />
+		</td>
+
+		<!-- 身分證字號 -->
+		<td>
+			<input name="" class="span12" type="text" />
+		</td>
+
+		<!-- 生日 -->
+		<td>
+			<input name="" class="span12" type="text" />
+		</td>
+
+		<!-- 就醫日期 -->
+		<td>
+			<input name="" class="span12" type="text" />
+		</td>
+
+		<!-- 處方箋天數 -->
+		<td>
+			<input name="" class="span12" type="text" />
+		</td>
+
+		<!-- 可調劑次數 -->
+		<td>
+			<select name="" class="span12">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+			</select>
+		</td>
+
+		<!-- 外送次數 -->
+		<td>
+			<label>1</label>
+			<input class="js-prescription-time-1" type="checkbox" value="1" />
+			<label>2</label>
+			<input class="js-prescription-time-2" type="checkbox" value="2" />
+			<label>3</label>
+			<input class="js-prescription-time-3" type="checkbox" value="3" />
+		</td>
+
+		<!-- 藥吃完日 -->
+		<td>
+			<span class="first-time" style="display: inline-block;">
+				<!-- js -->
+			</span>
+			<span class="second-time" style="display: inline-block;">
+				<!-- js -->
+			</span>
+		</td>
+
+		<!-- 處方箋上傳 -->
+		<td>
+			<!-- file -->
+		</td>
+
+		<!-- 處方箋取得方式 -->
+		<td>
+			<select class="span12 form-control js-status">
+				<option value="original" selected="">正本</option>
+				<option value="drug-dealer-photo">藥師拍照</option>
+				<option value="fax">傳真</option>
+				<option value="line">line</option>
+				<option value="upload">上傳檔案</option>
+			</select>
+		</td>
+
+		<!-- 複製/刪除 -->
+		<td>
+			<!-- js -->
+			<div class="btn-toolbar">
+				<div class="btn-group">
+					<button type="button" id="ButtonCopy" class="btn btn-default btn-sm" title="Suchen">
+						<span class="glyphicon glyphicon-file"></span>
+					</button>
+					<button type="button" class="btn btn-default btn-sm js-ButtonDelete" title="Suchen">
+						<span class="glyphicon glyphicon-trash"></span>
+					</button>
+				</div>
+			</div>
+		</td>
+
+		<!-- 備註 -->
+		<td>
+			<textarea></textarea>
+		</td>
+	</tr>
+	</tbody>
+</table>
