a87a8a4862a3a36038b37a255c936d6edbe0fd52
Simon Asika <asika32764@gmail.com>
2014-04-21 12:27:17 +0800

    Add table constants class

---
+++ administrator/components/com_schedule/src/Schedule/Table/Table.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Part of schedule project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Schedule\Table;
+
+/**
+ * Quick constants for every table's name.
+ *
+ * @since 1.0
+ */
+abstract class Table
+{
+	const ADDRESSES = '#__schedule_addresses';
+
+	const COLORS = '#__schedule_colors';
+
+	const CUSTOMERS = '#__schedule_customers';
+
+	const DRUGPRICES = '#__schedule_drugprices';
+
+	const DRUGS = '#__schedule_drugs';
+
+	const HOLIDAYS = '#__schedule_holidays';
+
+	const HOSPITALS = '#__schedule_hospitals';
+
+	const IMAGES = '#__schedule_images';
+
+	const INSTITUTES = '#__schedule_institutes';
+
+	const MEMBERS = '#__schedule_members';
+
+	const PRESCRIPTIONS = '#__schedule_prescriptions';
+
+	const ROUTES = '#__schedule_routes';
+
+	const SCHEDULES = '#__schedule_schedules';
+
+	const SENDERS = '#__schedule_senders';
+
+	const TASKS = '#__schedule_tasks';
+}
