241d5dbb359ac83cfbeb939293b3a88f7bd2e938
bblurock <llwbenson@gmail.com>
2014-04-23 13:43:46 +0800

    Fixed: sender_name in task edit view now working.

--- administrator/components/com_schedule/view/task/tmpl/edit.php
+++ administrator/components/com_schedule/view/task/tmpl/edit.php
@@ -24,6 +24,12 @@ $container = $this->getContainer();
 $form      = $data->form;
 $item      = $data->item;
 
+$fieldsets = $data->form->getFieldsets();
+$fieldset = $fieldsets['information'];
+
+$senderId = $form->getField('sender')->id;
+$senderName = $form->getField('sender_name')->id;
+
 ?>
 <!-- Validate Script -->
 <script type="text/javascript">
@@ -33,16 +39,39 @@ $item      = $data->item;
 		{
 			Joomla.submitform(task, document.getElementById('adminForm'));
 		}
-	}
+
+
+	};
+
+	jQuery(function()
+	{
+		var senderId = jQuery( "#" + "<?php echo $senderId?>" );
+		var senderName = jQuery( '#<?php echo $senderName?>' );
+
+		senderId.change( function(){
+			senderName.val( jQuery(this).find("option:selected").text() );
+
+		});
+	})
 </script>
 
 <div id="schedule" class="windwalker task edit-form row-fluid">
 	<form action="<?php echo JURI::getInstance(); ?>"  method="post" name="adminForm" id="adminForm"
 		class="form-validate" enctype="multipart/form-data">
+		<div class="row-fluid">
+			<div class="span8">
+				<fieldset id="sender-edit-fieldset-<?php echo $fieldset->name ?>" class="form-horizontal">
+					<legend>
+						<?php echo $fieldset->label ? JText::_($fieldset->label) : JText::_('COM_SCHEDULE_EDIT_FIELDSET_' . $fieldset->name); ?>
+					</legend>
 
-		<?php
-		echo $this->loadTemplate('tab_basic', array('tab' => 'tab_basic'));
-		?>
+					<?php foreach ($data->form->getFieldset($fieldset->name) as $field): ?>
+						<div id="control_<?php echo $field->id; ?>">
+							<?php echo $field->getControlGroup() . "\n\n"; ?>
+						</div>
+					<?php endforeach;?>
+				</fieldset>
+			</div>
 
 		<!-- Hidden Inputs -->
 		<div id="hidden-inputs">
