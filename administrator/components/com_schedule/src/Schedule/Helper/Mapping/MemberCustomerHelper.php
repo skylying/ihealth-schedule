f47eac87e714fd41ddbbd0623c5d1746aea88035
leo <leo@bm-sms.com.tw>
2014-04-25 17:30:56 +0800

    udpated `MemberCustomerHelper`

--- administrator/components/com_schedule/src/Schedule/Helper/Mapping/MemberCustomerHelper.php
+++ administrator/components/com_schedule/src/Schedule/Helper/Mapping/MemberCustomerHelper.php
@@ -18,30 +18,228 @@ class MemberCustomerHelper
 	const RELATION_TABLE = '#__schedule_customer_member_maps';
 
 	/**
-	 * updateCustomers
+	 * Load customers
 	 *
-	 * @param       $mid
-	 * @param array $cids
+	 * @param   int  $memberId
+	 *
+	 * @return  \stdClass[]
+	 */
+	public static function loadCustomers($memberId)
+	{
+		$db = \JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('*')
+			->from(static::RELATION_TABLE)
+			->where('member_id=' . $memberId);
+
+		return $db->setQuery($query)->loadObjectList();
+	}
+
+	/**
+	 * Load members
+	 *
+	 * @param   int  $customerId
+	 *
+	 * @return  \stdClass[]
+	 */
+	public static function loadMembers($customerId)
+	{
+		$db = \JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('*')
+			->from(static::RELATION_TABLE)
+			->where('customer_id=' . $customerId);
+
+		return $db->setQuery($query)->loadObjectList();
+	}
+
+	/**
+	 * Add customers
+	 *
+	 * @param   int    $memberId
+	 * @param   int[]  $customerIds
 	 *
 	 * @return  bool
 	 */
-	public static function updateCustomers($mid, $cids = array())
+	public static function connectCustomers($memberId, $customerIds)
 	{
-		if (empty($cids))
+		if (empty($customerIds))
+		{
+			return true;
+		}
+
+		$customerIds = array_unique($customerIds);
+
+		$db = \JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->insert(static::RELATION_TABLE)
+			->columns(array('member_id', 'customer_id'));
+
+		foreach ($customerIds as $customerId)
+		{
+			$query->values($memberId . ', ' . $customerId);
+		}
+
+		$db->transactionStart();
+
+		try
+		{
+			$db->setQuery($query)->execute();
+		}
+		catch (\Exception $e)
+		{
+			$db->transactionRollback();
+
+			return false;
+		}
+
+		$db->transactionCommit();
+
+		return true;
+	}
+
+	/**
+	 * Add members
+	 *
+	 * @param   int    $customerId
+	 * @param   int[]  $memberIds
+	 *
+	 * @return  bool
+	 */
+	public static function connectMembers($customerId, $memberIds)
+	{
+		if (empty($memberIds))
+		{
+			return true;
+		}
+
+		$memberIds = array_unique($memberIds);
+
+		$db = \JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->insert(static::RELATION_TABLE)
+			->columns(array('customer_id', 'member_id'));
+
+		foreach ($memberIds as $memberId)
+		{
+			$query->values($customerId . ', ' . $memberId);
+		}
+
+		$db->transactionStart();
+
+		try
+		{
+			$db->setQuery($query)->execute();
+		}
+		catch (\Exception $e)
+		{
+			$db->transactionRollback();
+
+			return false;
+		}
+
+		$db->transactionCommit();
+
+		return true;
+	}
+
+	/**
+	 * Delete customers
+	 *
+	 * @param   int  $memberId
+	 *
+	 * @return  bool
+	 */
+	public static function disconnectCustomers($memberId)
+	{
+		$db = \JFactory::getDbo();
+
+		$query = $db->getQuery(true);
+
+		$query->delete(static::RELATION_TABLE)
+			->where('member_id = ' . $memberId);
+
+		$db->transactionStart();
+
+		try
+		{
+			$db->setQuery($query)->execute();
+		}
+		catch (\Exception $e)
+		{
+			$db->transactionRollback();
+
+			return false;
+		}
+
+		$db->transactionCommit();
+
+		return true;
+	}
+
+	/**
+	 * Delete members
+	 *
+	 * @param   int  $customerId
+	 *
+	 * @return  bool
+	 */
+	public static function disconnectMembers($customerId)
+	{
+		$db = \JFactory::getDbo();
+
+		$query = $db->getQuery(true);
+
+		$query->delete(static::RELATION_TABLE)
+			->where('customer_id = ' . $customerId);
+
+		$db->transactionStart();
+
+		try
+		{
+			$db->setQuery($query)->execute();
+		}
+		catch (\Exception $e)
+		{
+			$db->transactionRollback();
+
+			return false;
+		}
+
+		$db->transactionCommit();
+
+		return true;
+	}
+
+	/**
+	 * updateCustomers
+	 *
+	 * @param   int    $memberId
+	 * @param   int[]  $customerIds
+	 *
+	 * @return  bool
+	 */
+	public static function updateCustomers($memberId, $customerIds = array())
+	{
+		if (empty($customerIds))
 		{
 			return true;
 		}
 
 		$app = \JFactory::getApplication();
 
-		if (!static::disconnectCustomers($mid))
+		if (!static::disconnectCustomers($memberId))
 		{
 			$app->enqueueMessage('COM_SCHEDULE_MEMBER_DISCONNECT_MAPPING_FAILED', 'error');
 
 			return false;
 		}
 
-		if (!static::connectCustomers($mid, $cids))
+		if (!static::connectCustomers($memberId, $customerIds))
 		{
 			$app->enqueueMessage('COM_SCHEDULE_MEMBER_CONNECT_MAPPING_FAILED', 'error');
 
@@ -52,63 +250,33 @@ class MemberCustomerHelper
 	}
 
 	/**
-	 * disconnectCustomers
+	 * updateMembers
 	 *
-	 * @param int $mid
-	 *
-	 * @return  bool
-	 */
-	public static function disconnectCustomers($mid)
-	{
-		$db = \JFactory::getDbo();
-
-		$query = $db->getQuery(true);
-
-		$query->delete(static::RELATION_TABLE)
-			->where('member_id = ' . $mid);
-
-		if (!$db->setQuery($query)->execute())
-		{
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * connectCustomers
-	 *
-	 * @param int   $mid
-	 * @param int[] $cids
+	 * @param   int    $customerId
+	 * @param   int[]  $memberIds
 	 *
 	 * @return  bool
 	 */
-	public static function connectCustomers($mid, $cids)
+	public static function updateMembers($customerId, $memberIds = array())
 	{
-		if (empty($cids))
+		if (empty($memberIds))
 		{
 			return true;
 		}
 
-		$cids = array_unique($cids);
+		$app = \JFactory::getApplication();
 
-		$db = \JFactory::getDbo();
-
-		$query = $db->getQuery(true);
-
-		$query->insert(static::RELATION_TABLE)
-			->columns(array('member_id', 'customer_id'));
-
-		array_map(
-			function ($cid) use ($query, $mid)
-			{
-				$query->values($mid . ', ' . $cid);
-			},
-			$cids
-		);
-
-		if (!$db->setQuery($query)->execute())
+		if (!static::disconnectMembers($customerId))
 		{
+			$app->enqueueMessage('COM_SCHEDULE_CUSTOMER_DISCONNECT_MAPPING_FAILED', 'error');
+
+			return false;
+		}
+
+		if (!static::connectMembers($customerId, $memberIds))
+		{
+			$app->enqueueMessage('COM_SCHEDULE_CUSTOMER_CONNECT_MAPPING_FAILED', 'error');
+
 			return false;
 		}
 
