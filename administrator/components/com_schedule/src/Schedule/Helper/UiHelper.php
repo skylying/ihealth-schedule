5a809121fa05113b90d2a4de999c25dadc1a8175
Simon Asika <asika32764@gmail.com>
2014-04-25 10:29:57 +0800

    Change link icon

--- administrator/components/com_schedule/src/Schedule/Helper/UiHelper.php
+++ administrator/components/com_schedule/src/Schedule/Helper/UiHelper.php
@@ -79,7 +79,7 @@ abstract class UiHelper
 			return '';
 		}
 
-		$title = $title . ' <small class="glyphicon glyphicon-share-alt"></small>';
+		$title = $title . ' <small class="glyphicon glyphicon-share"></small>';
 
 		$attrs['class'] = 'text-muted';
 		$attrs['target'] = '_blank';
