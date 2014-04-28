c0a08fcc40532cb6bd03156e724f2424d5990cf6
Lewis Jang <Lewis.AEdes@gmail.com>
2014-04-28 15:18:43 +0800

    fixed form

--- administrator/components/com_schedule/view/rxindividual/tmpl/new.php
+++ administrator/components/com_schedule/view/rxindividual/tmpl/new.php
@@ -6,11 +6,8 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-$basic            = $data->form->getFieldset("basic");
-$schedules["1st"] = $data->form->getFieldset("schedules_1st");
-$schedules["2nd"] = $data->form->getFieldset("schedules_2nd");
-$schedules["3rd"] = $data->form->getFieldset("schedules_3rd");
-$ps               = $data->form->getFieldset("schedules_ps");
+$basic = $data->form->getFieldset("basic");
+$ps    = $data->form->getFieldset("schedules_ps");
 
 ?>
 
@@ -41,7 +38,7 @@ $ps               = $data->form->getFieldset("schedules_ps");
 	}
 </style>
 
-<form class="form-horizontal">
+<form name="adminForm" id="adminForm" method="post" action="<?php echo JURI::getInstance(); ?>" class="form-horizontal">
 	<div class="row-fluid">
 		<div class="col-lg-5">
 			<?php
@@ -53,31 +50,32 @@ $ps               = $data->form->getFieldset("schedules_ps");
 		</div>
 		<div class="col-lg-5 col-lg-offset-1">
 			<?php foreach (array("1st", "2nd", "3rd") as $key): ?>
-			<div id="schedules_<?php echo $key; ?>" class="row-fluid schedules schedules_<?php echo $key; ?>">
-				<div class="col-lg-3">
-					<!-- TODO: 換成可愛的圓圈圈 -->
-					<?php echo $schedules[$key]['jform_deliver_nths']->getControlGroup(); ?>
-				</div>
-				<div class="col-lg-9">
-					<div class="row-fluid address">
-						<div class="col-lg-12">
-							<?php echo $schedules[$key]['jform_address']->getControlGroup(); ?>
+				<?php $schedules = $data->form->getGroup("schedules_{$key}"); ?>
+				<div id="schedules_<?php echo $key; ?>" class="row-fluid schedules schedules_<?php echo $key; ?>">
+					<div class="col-lg-3">
+						<!-- TODO: 換成可愛的圓圈圈 -->
+						<?php echo $schedules["jform_schedules_{$key}_deliver_nths"]->getControlGroup(); ?>
+					</div>
+					<div class="col-lg-9">
+						<div class="row-fluid address">
+							<div class="col-lg-12">
+								<?php echo $schedules["jform_schedules_{$key}_address"]->getControlGroup(); ?>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="col-lg-4">
+								<!-- TODO:js -->
+								<?php echo $schedules["jform_schedules_{$key}_empty_date"]->getControlGroup(); ?>
+							</div>
+							<div class="col-lg-4">
+								<?php echo $schedules["jform_schedules_{$key}_send_date"]->getControlGroup(); ?>
+							</div>
+							<div class="col-lg-4">
+								<?php echo $schedules["jform_schedules_{$key}_send_time"]->getControlGroup(); ?>
+							</div>
 						</div>
 					</div>
-					<div class="row-fluid">
-						<div class="col-lg-4">
-							<!-- TODO:js -->
-							<?php echo $schedules[$key]['jform_empty_date']->getControlGroup(); ?>
-						</div>
-						<div class="col-lg-4">
-							<?php echo $schedules[$key]['jform_send_time']->getControlGroup(); ?>
-						</div>
-						<div class="col-lg-4">
-							<?php echo $schedules[$key]['jform_send_date']->getControlGroup(); ?>
-						</div>
-					</div>
-				</div>
-			</div>
+                </div>
 			<?php endforeach; ?>
 			<div class="row-fluid well">
 				<div class="col-lg-12">
@@ -92,4 +90,8 @@ $ps               = $data->form->getFieldset("schedules_ps");
 			</div>
 		</div>
 	</div>
+
+    <input type="hidden" name="option" value="com_schedule" />
+    <input type="hidden" name="task" value="" />
+    <?php echo JHtml::_('form.token'); ?>
 </form>
