83a1d2c642e288c5271d3b1cfc0ad67c61438c4c
leo <leo@bm-sms.com.tw>
2014-04-29 19:12:32 +0800

    added a form field helper

---
+++ administrator/components/com_schedule/src/Schedule/Helper/Form/FieldHelper.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Part of schedule project.
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Schedule\Helper\Form;
+
+/**
+ * Class UiHelper
+ *
+ * @since 1.0
+ */
+abstract class FieldHelper
+{
+	/**
+	 * resetGroup
+	 *
+	 * @param  \JFormField  $field
+	 * @param  string       $group
+	 *
+	 * @return  \JFormField
+	 */
+	public static function resetGroup($field, $group)
+	{
+		// Modify field's group name
+		$field->group = $group;
+
+		// Update field's form name
+		$field->name = $field->fieldname;
+
+		return $field;
+	}
+}
