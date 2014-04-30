892a19e8741d4158b84b50a5654ab6828ecd82b5
leo <leo@bm-sms.com.tw>
2014-04-30 19:38:26 +0800

    updated rxresident controller "edit.add"

--- administrator/components/com_schedule/controller/rxresident/edit/add.php
+++ administrator/components/com_schedule/controller/rxresident/edit/add.php
@@ -1,29 +1,26 @@
 <?php
 
-use Windwalker\Controller\Admin\AbstractRedirectController;
+use Windwalker\Controller\Edit\AddController;
 
 /**
  * Class ScheduleControllerRxResidentEditAdd
  */
-class ScheduleControllerRxResidentEditAdd extends AbstractRedirectController
+class ScheduleControllerRxResidentEditAdd extends AddController
 {
 	/**
-	 * doExecute
+	 * redirectToItem
 	 *
-	 * @return  mixed
+	 * @param   string  $recordId
+	 * @param   string  $urlVar
+	 * @param   string  $msg
+	 * @param   string  $type
+	 *
+	 * @return  void
 	 */
-	protected function doExecute()
+	public function redirectToItem($recordId = null, $urlVar = 'id', $msg = null, $type = 'message')
 	{
-		$query = array(
-			'option' => $this->option,
-			'view' => strtolower($this->getName()),
-			'layout' => 'edit_list',
-		);
+		$this->input->set('layout', 'edit_list');
 
-		$url = JRoute::_('index.php?' . http_build_query($query), false);
-
-		$this->redirect($url);
-
-		return true;
+		parent::redirectToItem($recordId, $urlVar, $msg, $type);
 	}
 }
