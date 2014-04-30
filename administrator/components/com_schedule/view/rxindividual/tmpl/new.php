64e65120ba0780e39e3926b2b7fb4a84fa4383d5
bblurock <llwbenson@gmail.com>
2014-04-30 18:30:42 +0800

    Dynamic loading address while customer has been selected.

--- administrator/components/com_schedule/view/rxindividual/tmpl/new.php
+++ administrator/components/com_schedule/view/rxindividual/tmpl/new.php
@@ -24,68 +24,169 @@ $mobileID = $data->form->getField('mobile')->id;
 ?>
 
 <script type="text/javascript">
-	var telOfficeID	= "<?php echo $telOfficeID;?>";
-	var telHomeID 	= "<?php echo $telHomeID;?>";
-	var mobileID 	= "<?php echo $mobileID;?>";
+	var telOfficeID = "<?php echo $telOfficeID;?>";
+	var telHomeID = "<?php echo $telHomeID;?>";
+	var mobileID = "<?php echo $mobileID;?>";
+	// Update empty rows of addresses inputs
+	var addressesKeys = ["1st", "2nd", "3rd"];
 
-	(function($){
-		$.fn.customerAjax = function(id) {
-			var $that = $(this);
-			return this.each(function() {
-				jQuery.ajax({
-					type:"POST",
-					url: "index.php?option=com_schedule&task=customer.phone.json&id=" + id
-				}).done(function(cdata){
-					var cdata 		= jQuery.parseJSON( cdata );
-					var tel_office  = jQuery.parseJSON(cdata.tel_office);
-					var tel_home 	= jQuery.parseJSON(cdata.tel_home);
-					var mobile 		= jQuery.parseJSON(cdata.mobile);
-
-					$.fn.customerAjax.updateHtml( telOfficeID, tel_office);
-					$.fn.customerAjax.updateHtml( telHomeID, tel_home);
-					$.fn.customerAjax.updateHtml( mobileID, mobile);
-				});
-			});
-		};
-		$.fn.customerAjax.updateHtml = function( tagID , telJson ){
-
-			//Clear target hook html first.
-			$('#'+ tagID).find('.controls').html("");
-
-			for (var i = 0; i < telJson.length ; i++  )
+	(function ($)
+	{
+		$.fn.customerAjax = function (id)
+		{
+			return this.each(function ()
 			{
-				//Set Defalut radio button
-				var html = '<input type="radio" name="default_' + tagID + '_" ';
-				if (telJson[i].default == 'true')
+				if (!id)
 				{
-					html += "checked/>";
+					// Update empty rows of phone inputs
+					$.fn.customerAjax.updatePhoneHtml(telOfficeID);
+					$.fn.customerAjax.updatePhoneHtml(telHomeID);
+					$.fn.customerAjax.updatePhoneHtml(mobileID);
+
+					// There are three column to update
+					for (var i = 0; i < addressesKeys.length; i++)
+					{
+						$.fn.customerAjax.updateAddressHtml(addressesKeys[i], name);
+					}
 				}
 				else
 				{
-					html += ">";
+					$.fn.customerAjax.fireAjax(id);
 				}
+			});
+		};
+		$.fn.customerAjax.fireAjax = function (id)
+		{
+			// Fire ajax to Customer
+			jQuery.ajax({
+				type: "POST",
+				url: "index.php?option=com_schedule&task=customer.ajax.json&id=" + id
+			}).done(function (cdata)
+				{
+					var cdata = jQuery.parseJSON(cdata);
+					var tel_office = jQuery.parseJSON(cdata.tel_office);
+					var tel_home = jQuery.parseJSON(cdata.tel_home);
+					var mobile = jQuery.parseJSON(cdata.mobile);
 
-				// Set Numbers
-				html += '<input type="text" name="number_' +
-						tagID +
-						'" ' +
-						'value="' +
-						telJson[i].number +
+					$.fn.customerAjax.updatePhoneHtml(telOfficeID, tel_office);
+					$.fn.customerAjax.updatePhoneHtml(telHomeID, tel_home);
+					$.fn.customerAjax.updatePhoneHtml(mobileID, mobile);
+				});
+
+			//Fire ajax to Addresses
+			jQuery.ajax({
+				type: "POST",
+				url: "index.php?option=com_schedule&task=addresses.ajax.json&id=" + id
+			}).done(function (cdata)
+				{
+					var cdata = jQuery.parseJSON(cdata);
+
+					// Update empty rows of addresses inputs
+
+					for (var i = 0; i < addressesKeys.length; i++)
+					{
+						$.fn.customerAjax.updateAddressHtml(addressesKeys[i], cdata);
+					}
+				});
+		};
+
+		$.fn.customerAjax.updateAddressHtml = function (key, addressJson)
+		{
+			addressJson = addressJson || {};
+
+			// ex: jform_schedule_1st_address
+			var targetID = 'jform_schedules_' + key + '_address';
+
+			// ex: jform[schedule_1st][address]
+			var targetName = 'jform[' + 'schedule_' + key + '][address]';
+
+			// Find its parent, later we will replace it with new select list
+			var targetsParent = $('#' + targetID).parent();
+
+			var html = '';
+
+			// Add select tag
+			html += '<select' +
+				' name="' + targetName + '"' +
+				' id="' + targetID + '">';
+
+			for (var i = 0; i < addressJson.length; i++)
+			{
+				// Add option tag
+				html += '<option' +
+					' val="' + addressJson[i].id + '">' +
+					addressJson[i].city_title +
+					addressJson[i].area_title +
+					addressJson[i].address +
+					'</option>';
+			}
+
+			html += '</select>';
+
+			//Clear target hook's html first.
+			targetsParent.html("");
+
+			targetsParent.html(html);
+		};
+
+		$.fn.customerAjax.updatePhoneHtml = function (tagID, telJson)
+		{
+			telJson = telJson || {};
+
+			var target = $('#' + tagID);
+			var defaultLength = telJson.length ? telJson.length : 3;
+
+			//Clear target hook's html first.
+			target.find('.controls').html("");
+
+			//Update rows, append new input rows to target element
+			for (var i = 0; i < defaultLength; i++)
+			{
+				var html = '';
+
+				if (telJson[i] === undefined)
+				{
+					html += '<input type="radio" name="default_' + tagID + '"' + '/>';
+					html += '<input type="text" name="number_' + tagID + '"' + '/><br>'
+				}
+				else
+				{
+					//Set Defalut radio button
+					html += '<input type="radio" name="default_' + tagID + '"';
+					html += (telJson[i].default == 'true') ? ' checked/>' : '/>';
+
+					// Set Numbers
+					html += '<input type="text"' +
+						' name="number_' + tagID + '"' +
+						' value="' + (telJson[i].number ? telJson[i].number : '' ) +
 						'"/><br>';
-
-				$('#'+ tagID).find('.controls').append(html);
+				}
+				target.find('.controls').append(html);
 			}
 		};
 	})(jQuery);
 
-	jQuery(document).ready( function(){
+	jQuery(document).ready(function ()
+	{
 
+		// customer_id element id
 		var customerDropDown = jQuery("#" + "<?php echo $customerID;?>");
 
-		customerDropDown.customerAjax(2);
+		// customer_id value
+		var customerID = "<?php $data->form->getInput('customer_id');?>";
 
-		customerDropDown.on('change', function(){
-			jQuery(this).customerAjax(jQuery(this).val()) ;
+		// If customer id is not set, select the first option
+		if (!customerID)
+		{
+			customerID = customerDropDown.find('option:first').val();
+
+			// Update once on load.
+			customerDropDown.customerAjax(customerID);
+		}
+
+		customerDropDown.on('change', function ()
+		{
+			jQuery(this).customerAjax(jQuery(this).val());
 		});
 	});
 </script>
