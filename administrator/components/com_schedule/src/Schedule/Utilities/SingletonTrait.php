6139eb57110733c8d8e63dec3ef9c2c32c257440
Simon Asika <asika32764@gmail.com>
2014-05-01 14:12:17 +0800

    Add SdkHelper

---
+++ administrator/components/com_schedule/src/Schedule/Utilities/SingletonTrait.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of schedule project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Schedule\Utilities;
+
+/**
+ * Trait SingletonTrait
+ *
+ * @since 1.0
+ */
+trait SingletonTrait
+{
+	pub
+}
