4c7b2baa5c32eda29a647f6bf46f7786bba30fba
takeshiwayne <wayne@bm-sms.com.tw>
2014-05-05 11:34:33 +0800

    [Feature #87] 實作儲存 JSON 電話格式

--- administrator/components/com_schedule/view/customer/tmpl/edit_basic.php
+++ administrator/components/com_schedule/view/customer/tmpl/edit_basic.php
@@ -16,7 +16,6 @@ $typeField = $data->form->getField('type');
 $customerType = $data->item->type;
 
 ?>
-
 <div class="row-fluid">
 	<div class="span6">
 		<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['edit'], 'class' => 'form-horizontal')); ?>
@@ -27,7 +26,12 @@ $customerType = $data->item->type;
 		<div id="individualdiv" class="<?php echo $customerType == 'individual' ? '' : 'hide'; ?>">
 			<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['rxindividual'], 'class' => 'form-horizontal')); ?>
 			<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['address'], 'class' => 'form-horizontal')); ?>
-			<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['phones'], 'class' => 'form-horizontal')); ?>
+			<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['office'], 'class' => 'form-horizontal')); ?>
+			<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['home'], 'class' => 'form-horizontal')); ?>
+			<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['mobile'], 'class' => 'form-horizontal')); ?>
+		</div>
+		<div>
+
 		</div>
 		<div id="residentdiv" class="<?php echo $customerType == 'resident' ? '' : 'hide'; ?>">
 			<?php echo $this->loadTemplate('fieldset', array('fieldset' => $fieldsets['institute'], 'class' => 'form-horizontal')); ?>
@@ -55,7 +59,46 @@ $customerType = $data->item->type;
 				individualDiv.addClass('hide');
 				residentDiv.removeClass('hide');
 			}
-		})
+		});
+
+ 		var telOffice = jQuery('#jform_tel_office').val(),
+			telHome = jQuery('#jform_tel_home').val(),
+			telMobile = jQuery('#jform_mobile').val(),
+
+			jsonTelO = jQuery.parseJSON(telOffice),
+			jsonTelH = jQuery.parseJSON(telHome),
+			jsonTelM = jQuery.parseJSON(telMobile);
+
+		update = function(){
+
+			jsonTelO = [
+				{ "default" : jQuery('#radiojform_tel_office0').is(':checked'), "number" :  jQuery('#jform_tel_office0').val()} ,
+				{ "default" : jQuery('#radiojform_tel_office1').is(':checked'), "number" : jQuery('#jform_tel_office1').val()} ,
+				{ "default" : jQuery('#radiojform_tel_office2').is(':checked'), "number" :  jQuery('#jform_tel_office2').val()},
+			]
+
+			jsonTelH = [
+				{ "default" : jQuery('#radiojform_tel_home0').is(':checked'), "number" :  jQuery('#jform_tel_home0').val()} ,
+				{ "default" : jQuery('#radiojform_tel_home1').is(':checked'), "number" : jQuery('#jform_tel_home1').val()} ,
+				{ "default" : jQuery('#radiojform_tel_home2').is(':checked'), "number" :  jQuery('#jform_tel_home2').val()},
+			]
+
+			jsonTelM = [
+				{ "default" : jQuery('#radiojform_mobile0').is(':checked'), "number" :  jQuery('#jform_mobile0').val()} ,
+				{ "default" : jQuery('#radiojform_mobile1').is(':checked'), "number" : jQuery('#jform_mobile1').val()} ,
+				{ "default" : jQuery('#radiojform_mobile2').is(':checked'), "number" :  jQuery('#jform_mobile2').val()},
+			]
+
+			jQuery('#jform_tel_office').val(JSON.stringify(jsonTelO)),
+			jQuery('#jform_tel_home').val(JSON.stringify(jsonTelH)),
+			jQuery('#jform_mobile').val(JSON.stringify(jsonTelM));
+
+		}
+
+		jQuery('#customer-edit-fieldset-phones, #customer-edit-fieldset-home, #customer-edit-fieldset-mobile input').each(function(){
+			jQuery(this).on('change', update );
+		});
+
 
 	})(jQuery);
 
