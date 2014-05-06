06f4b2fcfc9b31d06e22333e3f113431c88aeb3c
leo <leo@bm-sms.com.tw>
2014-05-06 11:39:38 +0800

    added helper method to calculate schedule send date

--- administrator/components/com_schedule/src/Schedule/Helper/ScheduleHelper.php
+++ administrator/components/com_schedule/src/Schedule/Helper/ScheduleHelper.php
@@ -3,6 +3,7 @@
 namespace Schedule\Helper;
 
 use Windwalker\Data\Data;
+use Schedule\Table\Table;
 
 /**
  * Class ScheduleHelper
@@ -45,5 +46,73 @@ class ScheduleHelper
 		}
 
 		return '';
+	}
+
+	/**
+	 * Calculate schedule date
+	 *
+	 * 取得排程日期
+	 *   1. 日期 = 就醫日期 + 給藥天數 - 10天 + N天
+	 *   2. N天 = 找最近的星期幾送藥
+	 *   3. 排除假日
+	 *
+	 * @param   string  $seeDoctorDate  就醫日期
+	 * @param   int     $period         給藥天數
+	 * @param   string  $weekday        星期幾送藥 ('MON','TUE','WED','THU','FRI','SAT','SUN')
+	 *
+	 * @return  string
+	 */
+	public static function calculateSendDate($seeDoctorDate, $period, $weekday = '')
+	{
+		$db = \JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$weekday = strtoupper($weekday);
+		$weekdays = array('MON','TUE','WED','THU','FRI','SAT','SUN');
+		$maxHolidays = 3;
+		$maxSearchDays = 30;
+		$startAt = strtotime($seeDoctorDate) + (86400 * ($period - 10));
+
+		// Get default weekday
+		if (!in_array($weekday, $weekdays))
+		{
+			$weekday = $weekdays[0];
+		}
+
+		// Get holidays
+		$query->select('`date`')
+			->from(Table::HOLIDAYS)
+			->where('`state`=1')
+			->where('`weekday`=' . $db->q($weekday))
+			->where('`date`>' . $db->q(date('Y-m-d', $startAt)));
+
+		// Convert date string to timestamp
+		$holidays = array_map(
+			function ($val)
+			{
+				return strtotime($val);
+			},
+			$db->setQuery($query, 0, $maxHolidays)->loadColumn()
+		);
+
+		// Shift to first day
+		for ($i = 0; $i < $maxSearchDays; ++$i, $startAt += 86400)
+		{
+			if (strtoupper(date('D', $startAt)) === $weekday)
+			{
+				break;
+			}
+		}
+
+		// Find send date
+		for ($i = 0; $i < $maxSearchDays; ++$i, $startAt += (86400 * 7))
+		{
+			if (!in_array($startAt, $holidays))
+			{
+				break;
+			}
+		}
+
+		return date('Y-m-d', $startAt);
 	}
 }
