a671e0ad1a1daeff54cb54de8136793374c0e742
skylying <linsutim@gmail.com>
2014-05-12 22:30:37 +0800

    modify json response

--- administrator/components/com_schedule/controller/institutes/search/json.php
+++ administrator/components/com_schedule/controller/institutes/search/json.php
@@ -33,11 +33,44 @@ class ScheduleControllerInstitutesSearchJson extends DisplayController
 					'dropdowntext' => $item->institute_short_title,
 					'color'        => $item->color_hex,
 					'delivery_day' => $item->delivery_weekday,
+					'floor'        => $item->floor
 				);
 			},
 			$model->getItems()
 		);
 
-		jexit(json_encode($items));
+		// Preoare floor data that select2 need
+		$itemsWithFloors = array();
+
+		foreach ($items as $item)
+		{
+			$floorArray = explode(',', $item['floor']);
+
+			foreach ($floorArray as $value)
+			{
+				$itemsWithFloors[] = array(
+
+					/**
+					 * select2 在 onchange 時會把回傳的 "id" 塞進本身的 value 裏面
+					 * 但這邊因應同一間機構在 id 相同狀況下必須更新樓層欄位的需求
+					 * 必須把 ajax 回傳的 "id" 做出區隔 (在這邊是後綴上 $value)
+					 * 才能滿足 select2 "onchange" 的事件觸發, 否則同一間機構,
+					 * 不同樓層, 再怎麼選 id 都一樣, 無法觸發 onchange 事件
+					 */
+					'id'           => $item['id'] . $value,
+
+					/**
+					 * 在 select2 的 onchange callback 中再把這個真正的 id 塞回 value
+					 */
+					'instituteid'  => $item['id'],
+					'dropdowntext' => $item['dropdowntext'] . ' ' . $value,
+					'color'        => $item['color'],
+					'delivery_day' => $item['delivery_day'],
+					'floor'        => $value
+				);
+			}
+		}
+
+		jexit(json_encode($itemsWithFloors));
 	}
 }
