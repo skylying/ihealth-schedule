ea5d739f4b4fbd754f34a6ff570a7a6f556d6df0
skylying <linsutim@gmail.com>
2014-05-12 16:19:26 +0800

    bind reresident events by delegation

--- media/com_schedule/js/rxresident/edit-list.js
+++ media/com_schedule/js/rxresident/edit-list.js
@@ -47,146 +47,18 @@
 					pickTime: false
 				});
 
-				// 可調劑次數與處方箋外送次數連動
-				$row.find('.times').on('change', function()
-				{
-					var timesValue = $(this).find('option:selected').val(),
-						cb1 = $(this).closest('tr').find('input[value="1st"]'),
-						cb2 = $(this).closest('tr').find('input[value="2nd"]'),
-						cb3 = $(this).closest('tr').find('input[value="3rd"]');
-
-					switch (timesValue)
-					{
-						case '1':
-							cb1.attr('checked', true);
-
-							cb2.attr('checked', false);
-							cb2.attr('disabled', true);
-
-							cb3.attr('checked', false);
-							cb3.attr('disabled', true);
-
-							break;
-
-						case '2':
-							cb1.attr('checked', false);
-
-							cb2.attr('checked', true);
-							cb2.attr('disabled', false);
-
-							cb3.attr('checked', false);
-							cb3.attr('disabled', true);
-
-							break;
-
-						case '3':
-							cb1.attr('checked', false);
-
-							cb2.attr('checked', true);
-							cb2.attr('disabled', false);
-
-							cb3.attr('checked', true);
-							cb3.attr('disabled', false);
-
-							break;
-					}
-				});
-
-				// if "可調劑次數" = 1, hide "第二次藥品吃完日"
-				$row.find('.times').on('change', function(event)
-				{
-					var timesValue = $(event.target).val(),
-						finishDate2Span = $row.find('.emptydisplay2');
-
-					if (timesValue == '1')
-					{
-						finishDate2Span.addClass('hide');
-					}
-					else
-					{
-						finishDate2Span.removeClass('hide');
-					}
-
-				});
-
-				// Bind onchange to "就醫日期" & "給藥天數"
-				$row.find('.seedr').on('change', {row:$row}, updateFinishDate);
-				$row.find('.period').on('change', {row:$row}, updateFinishDate);
-
-				/**
-				 * Update "第一次吃完藥品日" & "第二次吃完藥品日"
-				 *
-				 * @param event
-				 *
-				 * @return void
-				 */
-				function updateFinishDate(event)
-				{
-					var $row = event.data.row;
-
-					var seeDrDateNode  = $row.find('.seedr input'),
-						seeDrDateValue = seeDrDateNode.val();
-
-					// if "就醫日期" not set, return
-					if (seeDrDateValue == 'undefined' || seeDrDateValue == '')
-					{
-						return;
-					}
-
-					var periodNode      = $row.find('.period'),
-						periodValue     = parseInt(periodNode.val());
-
-					var dateObj = new Date(seeDrDateValue);
-
-					// Set finishdate1 & finishdate2
-					var finishDate1 = dateObj.setDate(dateObj.getDate() + periodValue),
-						finishDate2 = dateObj.setDate(dateObj.getDate() + periodValue);
-
-					// update "藥吃完日" view column
-					$row.find('.emptydisplay1').text(getFormat(finishDate1, false));
-					$row.find('.emptydisplay2').text(getFormat(finishDate2, false));
-
-					// update "藥吃完日" form field
-					$row.find('.emptydate1').val(getFormat(finishDate1, true));
-					$row.find('.emptydate2').val(getFormat(finishDate2, true));
-
-					/**
-					 * Create date format MM-DD
-					 *
-					 * @param   {timestamp} date
-					 * @param   {boolean}   type , true => return full format
-					 *
-					 * @returns {string}
-					 */
-					function getFormat(date, type)
-					{
-						var dateObj = new Date(date),
-							year    = dateObj.getFullYear(),
-							month   = dateObj.getMonth() + 1,
-							day     = dateObj.getDate(),
-							short   = ((month < 10) ? '0'+ month : month) + '-' + ((day < 10) ? '0'+ day : day),
-							full    = year + '-' + short,
-							result;
-
-						if (type == true)
-						{
-							result = full;
-						}
-						else
-						{
-							result = short;
-						}
-
-						return result;
-					}
-				}
+				// Initialize select2
+				customer_id.select2Initialize($row.find('.customer-id'));
 			});
+
+			// Inject javascript to every element on page
+			this.injectJsToRow($panel);
 
 			// Add row
 			$('.button-add-row').click(function ()
 			{
 				var i,
-					amount = $('#new-row-number').val();
+					amount = $(this).val();
 
 				amount = isNaN(amount) ? 0 : amount;
 
@@ -194,6 +66,9 @@
 				{
 					handler.insert($('#row-template').html());
 				}
+
+				// Update total number of rows
+				$('#totalrow').text($('#rx-list tr').length - 1);
 			});
 
 			// Delete row
@@ -207,6 +82,144 @@
 			{
 				handler.duplicate($(this).closest('tr'));
 			});
+		},
+
+		// Inject javascript to every single row
+		injectJsToRow : function($panel)
+		{
+			// 可調劑次數與處方箋外送次數連動
+			$panel.on('change', '.times', function()
+			{
+				var timesValue = $(this).find('option:selected').val(),
+					cb1 = $(this).closest('tr').find('input[value="1st"]'),
+					cb2 = $(this).closest('tr').find('input[value="2nd"]'),
+					cb3 = $(this).closest('tr').find('input[value="3rd"]');
+
+				switch (timesValue)
+				{
+					case '1':
+						cb1.attr('checked', true);
+
+						cb2.attr('checked', false);
+						cb2.attr('disabled', true);
+
+						cb3.attr('checked', false);
+						cb3.attr('disabled', true);
+
+						break;
+
+					case '2':
+						cb1.attr('checked', false);
+
+						cb2.attr('checked', true);
+						cb2.attr('disabled', false);
+
+						cb3.attr('checked', false);
+						cb3.attr('disabled', true);
+
+						break;
+
+					case '3':
+						cb1.attr('checked', false);
+
+						cb2.attr('checked', true);
+						cb2.attr('disabled', false);
+
+						cb3.attr('checked', true);
+						cb3.attr('disabled', false);
+
+						break;
+				}
+			});
+
+			// if "可調劑次數" = 1, hide "第二次藥品吃完日"
+			$panel.on('change', '.times', function(event)
+			{
+				var $row = $($(this).closest('tr')),
+					timesValue = $(event.target).val(),
+					finishDate2Span = $row.find('.emptydisplay2');
+
+				if (timesValue == '1')
+				{
+					finishDate2Span.addClass('hide');
+				}
+				else
+				{
+					finishDate2Span.removeClass('hide');
+				}
+			});
+
+			// Bind onchange to "就醫日期" & "給藥天數"
+			$panel.on('change', '.seedr', updateFinishDate);
+			$panel.on('change', '.period', updateFinishDate);
+
+			/**
+			 * Update "第一次吃完藥品日" & "第二次吃完藥品日"
+			 *
+			 * @param event
+			 *
+			 * @return void
+			 */
+			function updateFinishDate(event)
+			{
+				var $row = $($(this).closest('tr'));
+
+				var seeDrDateNode  = $row.find('.seedr input'),
+					seeDrDateValue = seeDrDateNode.val();
+
+				// if "就醫日期" not set, return
+				if (seeDrDateValue == 'undefined' || seeDrDateValue == '')
+				{
+					return;
+				}
+
+				var periodNode      = $row.find('.period'),
+					periodValue     = parseInt(periodNode.val());
+
+				var dateObj = new Date(seeDrDateValue);
+
+				// Set finishdate1 & finishdate2
+				var finishDate1 = dateObj.setDate(dateObj.getDate() + periodValue),
+					finishDate2 = dateObj.setDate(dateObj.getDate() + periodValue);
+
+				// update "藥吃完日" view column
+				$row.find('.emptydisplay1').text(getFormat(finishDate1, false));
+				$row.find('.emptydisplay2').text(getFormat(finishDate2, false));
+
+				// update "藥吃完日" form field
+				$row.find('.emptydate1').val(getFormat(finishDate1, true));
+				$row.find('.emptydate2').val(getFormat(finishDate2, true));
+
+				/**
+				 * Create date format MM-DD
+				 *
+				 * @param   {timestamp} date
+				 * @param   {boolean}   type , true => return full format
+				 *
+				 * @returns {string}
+				 */
+				function getFormat(date, type)
+				{
+					var dateObj = new Date(date),
+						year    = dateObj.getFullYear(),
+						month   = dateObj.getMonth() + 1,
+						day     = dateObj.getDate(),
+						short   = ((month < 10) ? '0'+ month : month) + '-' + ((day < 10) ? '0'+ day : day),
+						full    = year + '-' + short,
+						result;
+
+					if (type == true)
+					{
+						result = full;
+					}
+					else
+					{
+						result = short;
+					}
+
+					return result;
+				}
+			}
 		}
 	};
 
@@ -230,7 +243,8 @@
 		};
 
 		var weekday = e.added.delivery_day,
-			color   = e.added.color;
+			color   = e.added.color,
+			hasInitialValue = e.added.hasInitialValue;
 
 		var weekdaySpan = $('#weekday-from-js'),
 			colorBlock  = $('.deliverycolor');
@@ -244,15 +258,25 @@
 		// Export institute_id
 		window.instituteId = e.added.id;
 
+		// For view = edit, we don't need to re-initialize select2
+		if (typeof hasInitialValue !== undefined && hasInitialValue == 'true')
+		{
+			return;
+		}
+
 		// If user change institute, initialize $row with select2 again
 		var panel = $('#rx-list').find('tr');
 
 		$(panel).each(function()
 		{
-			// TODO:customer_id 目前寫死
-			customer_id.initialize($(this).find('.customer-id'));
-		})
+			var $row = $(this);
 
+			// Initialize select2 with global object
+			customer_id.select2Initialize($row.find('.customer-id'));
+
+			$row.find('.idnumber').val('');
+			$row.find('.birthday input').val('');
+		})
 	};
 
 	/**
