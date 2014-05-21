d5f6d8fefca689078ef27fdd0971f8255b23e386
takeshiwayne <wayne@bm-sms.com.tw>
2014-05-21 18:12:23 +0800

    refactored radioinput code

--- administrator/components/com_schedule/model/field/radioinput.php
+++ administrator/components/com_schedule/model/field/radioinput.php
@@ -1,4 +1,7 @@
 <?php
+
+use Windwalker\Html\HtmlElement;
+
 /**
  * Part of ihealth project. 
  *
@@ -24,111 +27,69 @@ class JFormFieldRadioinput extends JFormField
 	protected $type = 'Radioinput';
 
 	/**
-	 * The allowable maxlength of the field.
-	 *
-	 * @var    integer
-	 * @since  3.2
-	 */
-	protected $maxLength;
-
-	/**
-	 * The mode of input associated with the field.
-	 *
-	 * @var    mixed
-	 * @since  3.2
-	 */
-	protected $inputmode;
-
-	/**
-	 * The name of the form field direction (ltr or rtl).
-	 *
-	 * @var    string
-	 * @since  3.2
-	 */
-	protected $dirname;
-
-	/**
 	 * getInput
 	 *
 	 * @return  string
 	 */
 	protected function getInput()
 	{
-		// Translate placeholder text
-		$hint = $this->translateHint ? JText::_($this->hint) : $this->hint;
-
 		// Initialize some field attributes.
-		$size         = !empty($this->size) ? ' size="' . $this->size . '"' : '';
-		$maxLength    = !empty($this->maxLength) ? ' maxlength="' . $this->maxLength . '"' : '';
 		$class        = !empty($this->class) ? ' class="' . $this->class . '"' : '';
-		$readonly     = $this->readonly ? ' readonly' : '';
-		$disabled     = $this->disabled ? ' disabled' : '';
-		$required     = $this->required ? ' required aria-required="true"' : '';
-		$hint         = $hint ? ' placeholder="' . $hint . '"' : '';
-		$autocomplete = !$this->autocomplete ? ' autocomplete="off"' : ' autocomplete="' . $this->autocomplete . '"';
-		$autocomplete = $autocomplete == ' autocomplete="on"' ? '' : $autocomplete;
-		$autofocus    = $this->autofocus ? ' autofocus' : '';
-		$spellcheck   = $this->spellcheck ? '' : ' spellcheck="false"';
-		$pattern      = !empty($this->pattern) ? ' pattern="' . $this->pattern . '"' : '';
-		$inputmode    = !empty($this->inputmode) ? ' inputmode="' . $this->inputmode . '"' : '';
-		$dirname      = !empty($this->dirname) ? ' dirname="' . $this->dirname . '"' : '';
-		$list         = '';
 
 		// Initialize JavaScript field attributes.
 		$onchange = !empty($this->onchange) ? ' onchange="' . $this->onchange . '"' : '';
 
 		// Including fallback code for HTML5 non supported browsers.
 		JHtml::_('jquery.framework');
-		JHtml::_('script', 'system/html5fallback.js', false, true);
 
-		if (!empty($options))
-		{
-			$html[] = JHtml::_('select.suggestionlist', $options, 'value', 'text', $this->id . '_datalist"');
-			$list   = ' list="' . $this->id . '_datalist"';
-		}
-
-		$html[] = '<input type="text" style="display:none;" name="' . $this->name . '" id="' . $this->id . '"' . $dirname . ' value="'
-			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . $readonly . $list
-			. $hint . $onchange . $maxLength . $required . $autocomplete . $autofocus . $spellcheck . $inputmode . $pattern . ' />';
+		// Invisible json telephone input
+		$html[] = '<input type="text" style="display:none;" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
+			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class
+			. $onchange . ' />';
 
 		$jsonString = json_decode($this->value);
 
+		// Use loops to load json data to into empty html input
 		for ($key = 0; $key <= 2; $key++)
 		{
+			// Get radio input html
+			$inputAttr = array('id' => $key, 'type' => 'radio', 'name' => "radioInput" . $this->id);
+			$Unchecked = new HtmlElement('input', '', $inputAttr);
+
 			if (empty($jsonString))
 			{
-				$html[] = ' 預設 <div style="margin:10px"><input type="radio"' . ' id="radio' . $this->id . $key . '"' . 'name="radioInput' . $this->id . '" />
-				<input type="text"' . ' id="' . $this->id . $key . '"
-				name="textInput' . $this->id . '" value="' . '"' . $class . $size . $disabled . $readonly . $list
-					. $hint . $onchange . $maxLength . $required . $autocomplete . $autofocus . $spellcheck . $inputmode . $pattern . ' /></div>';
+				$html[] = '<div class="form-group"> 預設 ' . $Unchecked .
+				' <input type="text" id="' . $this->id . $key . '"
+				name="textInput' . $this->id . '" value="' . '"' . $class
+				. $onchange . ' /></div>';
 			}
 		}
 
+		// Use loops to load html if the json data is not empty
 		foreach ((array) $jsonString as $key => $value)
 		{
-			$jsonString = json_decode($this->value);
+			// Get radio input html
+			$inputAttr = array('id' => $key, 'type' => 'radio', 'checked' => 'checked', 'name' => "radioInput" . $this->id);
+			$Checked = new HtmlElement('input', '', $inputAttr);
+
+			$inputAttr = array('id' => $key, 'type' => 'radio', 'name' => "radioInput" . $this->id);
+			$Unchecked = new HtmlElement('input', '', $inputAttr);
 
 			$default = $value->default;
 
-			if ($default == 'true')
+			$checkbox = $Unchecked;
+
+			// Check if the checkbox is checked
+			if ($default == '1')
 			{
-				$html[] = ' 預設 <div  style="margin:10px">
-				<input type="radio"' . ' id="radio' . $this->id . $key . '"' . 'name="radioInput' . $this->id . '"checked />
-				<input type="text"' . ' id="' . $this->id . $key . '"
-				name="textInput' . $this->id . '" value="' . $value->number . '"' . $class . $size . $disabled . $readonly . $list
-					. $hint . $onchange . $maxLength . $required . $autocomplete . $autofocus . $spellcheck . $inputmode . $pattern . ' />
-					</div>';
+				$checkbox = $Checked;
 			}
-			else
-			{
-				$html[] = ' 預設 <div style="margin:10px"><input type="radio"' . ' id="radio' . $this->id . $key . '"' . 'name="radioInput' . $this->id . '" />
-				<input type="text"' . ' id="' . $this->id . $key . '"
-				name="textInput' . $this->id . '" value="' . $value->number . '"' . $class . $size . $disabled . $readonly . $list
-					. $hint . $onchange . $maxLength . $required . $autocomplete . $autofocus . $spellcheck . $inputmode . $pattern . ' /></div>';
-			}
+
+			$html[] = '<div class="form-group"> 預設 ' . $checkbox . ' <input type="text" id="' . $this->id . $key . '"
+				name="textInput' . $this->id . '" value="' . $value->number . '"' . $class
+				. $onchange . ' /></div>';
 		}
 
 		return implode($html);
-
 	}
 }
