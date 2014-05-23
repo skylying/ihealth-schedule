2f552b0fad82e0b4de8ffedec45be79284e83a9f
leo <leo@bm-sms.com.tw>
2014-05-23 16:47:54 +0800

    added API "api/member"

---
+++ components/com_schedule/model/member.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\Model\Member;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelMember
+ *
+ * @since 1.0
+ */
+class ScheduleModelMember extends Member
+{
+}
