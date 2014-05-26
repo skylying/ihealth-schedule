05a9802e989704e72ce638239ded3c1d81483999
Simon Asika <asika32764@gmail.com>
2014-05-26 14:43:48 +0800

    Add holiday list and filters

---
+++ components/com_schedule/model/holidays.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\Table\Table;
+use Windwalker\Model\Helper\QueryHelper;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelMember
+ *
+ * @since 1.0
+ */
+class ScheduleModelHolidays extends \Windwalker\Model\ListModel
+{
+	/**
+	 * Property filteerFields.
+	 *
+	 * @var  array
+	 */
+	protected $filteerFields = array(
+		'start_date', 'end_date'
+	);
+
+	/**
+	 * configureTables
+	 *
+	 * @return  void
+	 */
+	protected function configureTables()
+	{
+		$this->addTable('holiday', Table::HOLIDAYS);
+	}
+
+	/**
+	 * populateState
+	 *
+	 * @param string $ordering
+	 * @param string $direction
+	 *
+	 * @return  void
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		parent::populateState($ordering, $direction);
+
+		$container = $this->getContainer();
+
+		$input = $container->get('input');
+
+		// Prepare Start and End
+		$start = $input->get('start');
+		$end = $input->get('end');
+
+		$filters = $this->state->get('filter', array());
+
+		if ($start)
+		{
+			$filters['start_date'] = $start;
+		}
+
+		if ($end)
+		{
+			$filters['end_date'] = $end;
+		}
+
+		$this->state->set('filter', $filters);
+	}
+
+	/**
+	 * postGetQuery
+	 *
+	 * @param JDatabaseQuery $query
+	 *
+	 * @return  void
+	 */
+	protected function postGetQuery(\JDatabaseQuery $query)
+	{
+		$queryHelper = $this->container->get('model.' . $this->getName() . '.helper.query');
+
+		// Reset select to avoid redundant columns
+		$query->clear('select')
+			->select($queryHelper->getSelectFields(QueryHelper::COLS_WITH_FIRST));
+	}
+
+	/**
+	 * configureFilters
+	 *
+	 * @param \Windwalker\Model\Filter\FilterHelper $filterHelper
+	 *
+	 * @return  void
+	 */
+	protected function configureFilters($filterHelper)
+	{
+		// For Start day
+		$filterHelper->setHandler(
+			'start_date',
+			function($query, $field, $value)
+			{
+				$date = new \JDate($value);
+
+				/** @var $query \JDatabaseQuery */
+				$query->where($query->format('%n >= %q', 'year', $date->year))
+					->where($query->format('%n >= %q', 'month', (int) $date->month))
+					->where($query->format('%n >= %q', 'day', (int) $date->day));
+			}
+		);
+
+		// For End day
+		$filterHelper->setHandler(
+			'end_date',
+			function($query, $field, $value)
+			{
+				$date = new \JDate($value);
+
+				/** @var $query \JDatabaseQuery */
+				$query->where($query->format('%n <= %q', 'year', $date->year))
+					->where($query->format('%n <= %q', 'month', (int) $date->month))
+					->where($query->format('%n <= %q', 'day', (int) $date->day));
+			}
+		);
+	}
+}
