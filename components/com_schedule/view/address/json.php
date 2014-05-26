ab80acc7ded712b3ff573ec2da6040b83234240f
Simon Asika <asika32764@gmail.com>
2014-05-26 14:06:05 +0800

    Address API

---
+++ components/com_schedule/view/address/json.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\View\ApiView;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleViewMemberJson
+ *
+ * @since 1.0
+ */
+class ScheduleViewAddressJson extends ApiView
+{
+	/**
+	 * Property prefix.
+	 *
+	 * @var  string
+	 */
+	protected $prefix = 'schedule';
+
+	/**
+	 * Property option.
+	 *
+	 * @var  string
+	 */
+	protected $option = 'com_schedule';
+
+	/**
+	 * Property textPrefix.
+	 *
+	 * @var string
+	 */
+	protected $textPrefix = 'COM_SCHEDULE';
+
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'address';
+
+	/**
+	 * Property viewItem.
+	 *
+	 * @var  string
+	 */
+	protected $viewItem = 'address';
+
+	/**
+	 * Property viewList.
+	 *
+	 * @var  string
+	 */
+	protected $viewList = 'addresses';
+
+	/**
+	 * prepareData
+	 *
+	 * @throws  \Exception
+	 * @return  void
+	 */
+	protected function prepareData()
+	{
+		$data = $this->getData();
+
+		$data['item'] = $this->get('Item');
+
+		if (! $data['item']->id)
+		{
+			throw new \Exception('Item not found', 404);
+		}
+	}
+}
