5de9da767a8e868d169301a4e39c3029e0845943
Simon Asika <asika32764@gmail.com>
2014-05-26 12:39:59 +0800

    Move logic code to global DeleteController

--- administrator/components/com_schedule/src/Schedule/Controller/Api/ApiDeleteController.php
+++ administrator/components/com_schedule/src/Schedule/Controller/Api/ApiDeleteController.php
@@ -18,6 +18,45 @@ use Windwalker\Controller\State\DeleteController;
 class ApiDeleteController extends DeleteController
 {
 	/**
+	 * prepareExecute
+	 *
+	 * @return  void
+	 */
+	protected function prepareExecute()
+	{
+		parent::prepareExecute();
+
+		$id = $this->input->get('id');
+
+		if ($id)
+		{
+			$this->cid = array($id);
+		}
+	}
+
+	/**
+	 * Pose execute hook.
+	 *
+	 * @param   mixed $return Executed return value.
+	 *
+	 * @throws  \Exception
+	 * @return  mixed
+	 */
+	protected function postExecute($return = null)
+	{
+		if (! $return)
+		{
+			throw new \Exception('Delete fail.');
+		}
+
+		$state = $this->model->getState();
+
+		$this->setMessage('Delete Success.');
+
+		return $this->fetch('Schedule', $this->name . '.display', array('id' => $state->get($this->name . '.id')));
+	}
+
+	/**
 	 * Check session token or die.
 	 *
 	 * @return void
--- components/com_schedule/controller/member/state/delete.php
+++ components/com_schedule/controller/member/state/delete.php
@@ -15,42 +15,4 @@ use Schedule\Controller\Api\ApiDeleteController;
  */
 class ScheduleControllerMemberStateDelete extends ApiDeleteController
 {
-	/**
-	 * prepareExecute
-	 *
-	 * @return  void
-	 */
-	protected function prepareExecute()
-	{
-		parent::prepareExecute();
-
-		$id = $this->input->get('id');
-
-		if ($id)
-		{
-			$this->cid = array($id);
-		}
-	}
-
-	/**
-	 * Pose execute hook.
-	 *
-	 * @param   mixed $return Executed return value.
-	 *
-	 * @throws  \Exception
-	 * @return  mixed
-	 */
-	protected function postExecute($return = null)
-	{
-		if (! $return)
-		{
-			throw new \Exception('Delete fail.');
-		}
-
-		$state = $this->model->getState();
-
-		$this->setMessage('Delete Success.');
-
-		return $this->fetch('Schedule', $this->name . '.display', array('id' => $state->get($this->name . '.id')));
-	}
 }
