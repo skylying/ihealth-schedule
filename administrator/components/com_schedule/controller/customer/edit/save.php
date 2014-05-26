2b6e7ae913e9243d5999f5619009ad3393cc0332
Lewis Jang <Lewis.AEdes@gmail.com>
2014-05-26 17:49:53 +0800

    customer save city and area

--- administrator/components/com_schedule/controller/customer/edit/save.php
+++ administrator/components/com_schedule/controller/customer/edit/save.php
@@ -19,7 +19,20 @@ class ScheduleControllerCustomerEditSave extends SaveController
 	 */
 	protected function preSaveHook()
 	{
+		// 身分證字號大寫
 		$this->data['id_number'] = isset($this->data['id_number']) ? ucfirst($this->data['id_number']) : "";
+
+		$addresses = isset($this->data['address']) ? json_decode($this->data['address']) : array();
+
+		// 預設地址存入客戶資料
+		foreach ($addresses as $address)
+		{
+			if ($address->previous)
+			{
+				$this->data['city'] = $address->city;
+				$this->data['area'] = $address->area;
+			}
+		}
 
 		parent::preSaveHook();
 	}
