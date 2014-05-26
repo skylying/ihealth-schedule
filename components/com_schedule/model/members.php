83012848263f04677f82698aff966f30c5d16a8a
Simon Asika <asika32764@gmail.com>
2014-05-26 10:42:21 +0800

    Add member id and member list

---
+++ components/com_schedule/model/members.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\Table\Table;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelMember
+ *
+ * @since 1.0
+ */
+class ScheduleModelMembers extends \Windwalker\Model\ListModel
+{
+	/**
+	 * configureTables
+	 *
+	 * @return  void
+	 */
+	protected function configureTables()
+	{
+		$this->addTable('mamber', Table::MEMBERS);
+	}
+}
---
+++ components/com_schedule/view/members/json.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\View\ApiView;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleViewMemberJson
+ *
+ * @since 1.0
+ */
+class ScheduleViewMembersJson extends ApiView
+{
+	/**
+	 * Property prefix.
+	 *
+	 * @var  string
+	 */
+	protected $prefix = 'schedule';
+
+	/**
+	 * Property option.
+	 *
+	 * @var  string
+	 */
+	protected $option = 'com_schedule';
+
+	/**
+	 * Property textPrefix.
+	 *
+	 * @var string
+	 */
+	protected $textPrefix = 'COM_SCHEDULE';
+
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'members';
+
+	/**
+	 * Property viewItem.
+	 *
+	 * @var  string
+	 */
+	protected $viewItem = 'member';
+
+	/**
+	 * Property viewList.
+	 *
+	 * @var  string
+	 */
+	protected $viewList = 'members';
+
+	/**
+	 * prepareData
+	 *
+	 * @return  void
+	 */
+	protected function prepareData()
+	{
+		$data = $this->getData();
+
+		$data['items'] = $this->get('Items');
+	}
+}
