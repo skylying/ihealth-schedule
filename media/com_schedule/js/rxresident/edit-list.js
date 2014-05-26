8ec7c9deae53c99b33f7f4ca7ee47fd92fec57aa
skylying <linsutim@gmail.com>
2014-05-26 17:02:00 +0800

    fix resident birthday auto complete issue

--- media/com_schedule/js/rxresident/edit-list.js
+++ media/com_schedule/js/rxresident/edit-list.js
@@ -310,7 +310,7 @@
 	window.updateIdBirthday = function(e, $node)
 	{
 		var idNumber = $node.closest('tr').find('.idnumber'),
-			birthDay = $node.closest('tr').find('.birthday input');
+			birthDay = $node.closest('tr').find('.birthday');
 
 		idNumber.val(e.added.id_number);
 		birthDay.val(e.added.birth_date);
