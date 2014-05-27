60a5bca82d0184639fef3f788120febf8321a460
skylying <linsutim@gmail.com>
2014-05-27 18:29:34 +0800

    依照 Member 的規則將 Customer 的 Model 改換位置, 並將原本 loadForm data 的部分移到 getItem()

---
+++ administrator/components/com_schedule/src/Schedule/Model/Customer.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Schedule\Model;
+
+use Windwalker\Model\AdminModel;
+use Windwalker\Joomla\DataMapper\DataMapper;
+use Schedule\Table\Table;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelCustomer
+ *
+ * @since 1.0
+ */
+class Customer extends AdminModel
+{
+	/**
+	 * Property prefix.
+	 *
+	 * @var  string
+	 */
+	protected $prefix = 'schedule';
+
+	/**
+	 * Property option.
+	 *
+	 * @var  string
+	 */
+	protected $option = 'com_schedule';
+
+	/**
+	 * Property textPrefix.
+	 *
+	 * @var string
+	 */
+	protected $textPrefix = 'COM_SCHEDULE';
+
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'customer';
+
+	/**
+	 * Property viewItem.
+	 *
+	 * @var  string
+	 */
+	protected $viewItem = 'customer';
+
+	/**
+	 * Property viewList.
+	 *
+	 * @var  string
+	 */
+	protected $viewList = 'customers';
+
+	/**
+	 * Method to set new item ordering as first or last.
+	 *
+	 * @param   \JTable $table    Item table to save.
+	 * @param   string  $position 'first' or other are last.
+	 *
+	 * @return  void
+	 */
+	public function setOrderPosition($table, $position = 'last')
+	{
+		parent::setOrderPosition($table, $position);
+	}
+
+	/**
+	 * getItem
+	 *
+	 * @param   int  $pk
+	 *
+	 * @return  mixed
+	 */
+	public function getItem($pk = null)
+	{
+		$this->item = parent::getItem($pk);
+		$db    = \JFactory::getDbo();
+
+		if (empty($this->item->id))
+		{
+			return $this->item;
+		}
+
+		$query = $db->getQuery(true);
+
+		// Get member id list
+		// ==================
+		$query->select('`map`.`member_id`')
+			->from("#__schedule_customers AS customer")
+			->join('LEFT', $db->quoteName('#__schedule_customer_member_maps') . ' AS map ON customer.id = map.customer_id')
+			->where('`map`.`customer_id`= ' . $db->q($this->item->id));
+
+		// Inject member ids
+		$this->item->members = $db->setQuery($query)->loadColumn();
+
+		// Get full address list
+		// ==================
+		$addressMapper = new DataMapper(Table::ADDRESSES);
+
+		// Prepare empty string as json format
+		$this->item->address = '[]';
+
+		if (!empty($this->item->id))
+		{
+			$addressDataSet = $addressMapper->find(array("customer_id" => $this->item->id));
+
+			$addressReturnData = array();
+
+			foreach ($addressDataSet as $addressData)
+			{
+				$addressReturnData[] = $addressData;
+			}
+
+			$this->item->address = json_encode($addressReturnData);
+		}
+
+		return $this->item;
+	}
+
+	/**
+	 * prepareTable
+	 *
+	 * @param \JTable $table
+	 *
+	 * @return  void
+	 */
+	public function prepareTable(\JTable $table)
+	{
+		$jformData = \JFactory::getApplication()->input->get('jform', '', 'array');
+
+		$customerType = \Windwalker\Helper\ArrayHelper::getValue($jformData, 'type');
+
+		if ('individual' === $customerType)
+		{
+			$this->prepareCityTable($table);
+		}
+		else
+		{
+			$this->prepareInstituteTable($table);
+		}
+
+		parent::prepareTable($table);
+	}
+
+	/**
+	 * Prepare and sanitise the table data prior to save institute data.
+	 *
+	 * @param   \JTable  $table  A reference to a JTable object.
+	 *
+	 * @return  void
+	 */
+	public function prepareCityTable($table)
+	{
+		$tableCity = $this->getTable('City');
+		$tableCity->load($table->city);
+
+		$tableArea = $this->getTable('Area');
+		$tableArea->load($table->area);
+
+		$table->city_title = $tableCity->title;
+		$table->area_title = $tableArea->title;
+	}
+
+	/**
+	 * Prepare and sanitise the table data prior to save institute data.
+	 *
+	 * @param   \JTable  $table  A reference to a JTable object.
+	 *
+	 * @return  void
+	 */
+	public function prepareInstituteTable($table)
+	{
+		$tableInstitute = $this->getTable('Institute');
+		$tableInstitute->load($table->institute_id);
+
+		$table->city_title = $tableInstitute->city_title;
+		$table->area_title = $tableInstitute->area_title;
+	}
+}
