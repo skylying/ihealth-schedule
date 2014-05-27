ea41a9e476b56439bc42499ef1044a2e12da4eb1
leo <leo@bm-sms.com.tw>
2014-05-27 11:02:00 +0800

    removed ',' character inputs, and fixed some code styles

--- administrator/components/com_schedule/model/field/tagsinput.php
+++ administrator/components/com_schedule/model/field/tagsinput.php
@@ -9,10 +9,15 @@
 // No direct access
 defined('_JEXEC') or die;
 
+use Windwalker\Helper\XmlHelper;
+
 JFormHelper::loadFieldClass('list');
 
 /**
  * Class JFormFieldTagsInput
+ *
+ * XML properties:
+ * - minimum_input_length: (optional) Minimum input length, default is 2
  */
 class JFormFieldTagsInput extends JFormFieldList
 {
@@ -36,20 +41,33 @@ class JFormFieldTagsInput extends JFormFieldList
 		$id    = isset($this->element['id']) ? $this->element['id'] : null;
 		$cssId = '#' . $this->getId($id, $this->element['name']);
 
+		// Get minimum input length
+		$minimumInputLength = (int) XmlHelper::get($this->element, 'minimum_input_length', 2);
+
 		$this->__set('multiple', 'true');
 		$this->__set('name', $this->fieldname);
 
 		JHtmlFormbehavior::chosen($cssId);
 
 		JFactory::getDocument()->addScriptDeclaration("
-			(function($){
-				$(document).ready(function () {
+			(function($) {
+				$(document).ready(function() {
+					// Removed selected options
+					$('" . $cssId . " option').each(function()
+					{
+						var option = $(this);
+						if (option.text() === '')
+						{
+							option.remove();
+						}
+					});
+
 					// Method to add tags pressing enter
-					$('" . $cssId . "_chzn input').keyup(function(event) {
-
-						// Tag is greater than 3 chars and enter pressed
-						if (this.value.length >= 2 && (event.which === 13 || event.which === 188)) {
-
+					$('" . $cssId . "_chzn input').keyup(function(event)
+					{
+						// Tag is greater than " . $minimumInputLength . " chars and enter (or ',') key pressed
+						if (this.value.length >= " . $minimumInputLength . " && (event.which === 13 || event.which === 188))
+						{
 							// Search an highlighted result
 							var highlighted = $('" . $cssId . "_chzn').find('li.active-result.highlighted').first();
 
@@ -57,28 +75,34 @@ class JFormFieldTagsInput extends JFormFieldList
 							if (event.which === 13 && highlighted.text() !== '')
 							{
 								// Extra check. If we have added a custom tag with this text remove it
-								$('" . $cssId . " option').filter(function () { return $(this).val() == highlighted.text(); }).remove();
+								$('" . $cssId . " option').filter(function() { return $(this).val() == highlighted.text(); }).remove();
 
 								// Select the highlighted result
-								var tagOption = $('" . $cssId . " option').filter(function () { return $(this).html() == highlighted.text(); });
-								tagOption.attr('selected', 'selected');
+								var tagOption = $('" . $cssId . " option').filter(function() { return $(this).html() == highlighted.text(); });
+
+								tagOption.attr('selected', 'selected');console.log('highlighted');
 							}
 							// Add the custom tag option
 							else
 							{
+								// Remove all ',' characters
+								this.value = this.value.replace(/,/g, '');
+
 								var customTag = this.value;
 
 								// Extra check. Search if the custom tag already exists (typed faster than AJAX ready)
-								var tagOption = $('" . $cssId . " option').filter(function () { return $(this).html() == customTag; });
+								var tagOption = $('" . $cssId . " option').filter(function() { return $(this).html() == customTag; });
+
 								if (tagOption.text() !== '')
 								{
 									tagOption.attr('selected', 'selected');
 								}
-								else
+								else if (this.value !== '')
 								{
 									var option = $('<option>');
+
 									option.text(this.value).val(this.value);
-									option.attr('selected','selected');
+									option.attr('selected', 'selected');
 
 									// Append the option an repopulate the chosen field
 									$('" . $cssId . "').append(option);
@@ -86,9 +110,10 @@ class JFormFieldTagsInput extends JFormFieldList
 							}
 
 							this.value = '';
-							$('" . $cssId . "').trigger('liszt:updated');
-							event.preventDefault();
 
+							$('" . $cssId . "').trigger('liszt:updated');
+
+							event.preventDefault();
 						}
 					});
 				});
