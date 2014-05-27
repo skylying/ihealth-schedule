23023e7d8650c30dc0ed123107d4ea94915ccbe4
skylying <linsutim@gmail.com>
2014-05-27 18:33:07 +0800

    add customer [GET] api

---
+++ components/com_schedule/model/customer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\Model\Customer;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelCustomer
+ *
+ * @since 1.0
+ */
+class ScheduleModelCustomer extends Customer
+{
+	// Override any function if need
+}
---
+++ components/com_schedule/view/customer/json.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\View\ApiView;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleViewCustomerJson
+ *
+ * @since 1.0
+ */
+class ScheduleViewCustomerJson extends ApiView
+{
+	/**
+	 * Property prefix.
+	 *
+	 * @var  string
+	 */
+	protected $prefix = 'schedule';
+
+	/**
+	 * Property option.
+	 *
+	 * @var  string
+	 */
+	protected $option = 'com_schedule';
+
+	/**
+	 * Property textPrefix.
+	 *
+	 * @var string
+	 */
+	protected $textPrefix = 'COM_SCHEDULE';
+
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'customer';
+
+	/**
+	 * Property viewItem.
+	 *
+	 * @var  string
+	 */
+	protected $viewItem = 'customer';
+
+	/**
+	 * Property viewList.
+	 *
+	 * @var  string
+	 */
+	protected $viewList = 'customers';
+
+	/**
+	 * prepareData
+	 *
+	 * @throws  \Exception
+	 * @return  void
+	 */
+	protected function prepareData()
+	{
+		$data = $this->getData();
+
+		$data['item'] = $this->get('Item');
+
+		if (! $data['item']->id)
+		{
+			throw new \Exception('Item not found', 404);
+		}
+	}
+}
