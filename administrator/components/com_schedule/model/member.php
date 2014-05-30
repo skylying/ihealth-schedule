85fde5d4e8bbc9e38e02b6b29d9f8f1c3582f648
leo <leo@bm-sms.com.tw>
2014-05-30 14:47:31 +0800

    separate "getItem" method in front-end and back-end

--- administrator/components/com_schedule/model/member.php
+++ administrator/components/com_schedule/model/member.php
@@ -18,4 +18,35 @@ defined('_JEXEC') or die;
  */
 class ScheduleModelMember extends Member
 {
+	/**
+	 * Overwrite getItem in order to join mapping table
+	 *
+	 * @param null $pk
+	 *
+	 * @return  mixed
+	 */
+	public function getItem($pk = null)
+	{
+		$this->item = parent::getItem($pk);
+
+		if (empty($this->item->id))
+		{
+			return $this->item;
+		}
+
+		$db     = \JFactory::getDbo();
+		$query  = $db->getQuery(true);
+		$select = "`customer`.`id`";
+
+		$query->select($select)
+			->from("#__schedule_customers AS customer")
+			->join('LEFT', $db->quoteName('#__schedule_customer_member_maps') . ' AS map ON customer.id = map.customer_id')
+			->where("`map`.`member_id`= {$this->item->id}");
+
+		$db->setQuery($query);
+
+		$this->item->customer_id_list = $db->loadColumn();
+
+		return $this->item;
+	}
 }
--- administrator/components/com_schedule/src/Schedule/Model/Member.php
+++ administrator/components/com_schedule/src/Schedule/Model/Member.php
@@ -74,36 +74,4 @@ class Member extends AdminModel
 	{
 		parent::setOrderPosition($table, $position);
 	}
-
-	/**
-	 * Overwrite getItem in order to join mapping table
-	 *
-	 * @param null $pk
-	 *
-	 * @return  mixed
-	 */
-	public function getItem($pk = null)
-	{
-		$this->item = parent::getItem($pk);
-
-		if (empty($this->item->id))
-		{
-			return $this->item;
-		}
-
-		$db     = \JFactory::getDbo();
-		$query  = $db->getQuery(true);
-		$select = "`customer`.`id`";
-
-		$query->select($select)
-			->from("#__schedule_customers AS customer")
-			->join('LEFT', $db->quoteName('#__schedule_customer_member_maps') . ' AS map ON customer.id = map.customer_id')
-			->where("`map`.`member_id`= {$this->item->id}");
-
-		$db->setQuery($query);
-
-		$this->item->customer_id_list = $db->loadColumn();
-
-		return $this->item;
-	}
 }
