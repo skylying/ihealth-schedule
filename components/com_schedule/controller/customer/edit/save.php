cedbe792ca8793da6798d423b65d6236069a3782
leo <leo@bm-sms.com.tw>
2014-06-03 18:35:52 +0800

    updated controller of API "[POST] api/customer"

--- components/com_schedule/controller/customer/edit/save.php
+++ components/com_schedule/controller/customer/edit/save.php
@@ -7,6 +7,7 @@
  */
 
 use Schedule\Controller\Api\ApiSaveController;
+use Schedule\Helper\Mapping\MemberCustomerHelper;
 
 /**
  * Class ScheduleControllerCustomerEditSave
@@ -15,4 +16,52 @@ use Schedule\Controller\Api\ApiSaveController;
  */
 class ScheduleControllerCustomerEditSave extends ApiSaveController
 {
+	/**
+	 * preSaveHook
+	 *
+	 * @return  void
+	 */
+	protected function preSaveHook()
+	{
+		parent::preSaveHook();
+
+		// Restrict the customer type to "individual"
+		$this->data['type'] = 'individual';
+	}
+
+	/**
+	 * Method that allows child controller access to model data
+	 * after the data has been saved.
+	 *
+	 * @param   \Windwalker\Model\CrudModel  $model      The data model object.
+	 * @param   array                        $validData  The validated data.
+	 *
+	 * @return  void
+	 */
+	protected function postSaveHook($model, $validData)
+	{
+		parent::postSaveHook($model, $validData);
+
+		if (empty($validData['id']))
+		{
+			$validData['id'] = $model->getState()->get('customer.id');
+		}
+
+		$members = MemberCustomerHelper::loadMembers($validData['id']);
+		$memberIds = JArrayHelper::getColumn($members, 'id');
+
+		// Update customer-member mapping
+		MemberCustomerHelper::updateMembers($validData['id'], $memberIds);
+
+		// Get address model
+		$addressModel = $this->getModel("Address");
+
+		// Save address
+		foreach ($this->data['addresses'] as $address)
+		{
+			$address['customer_id'] = $validData['id'];
+
+			$addressModel->save($address);
+		}
+	}
 }
