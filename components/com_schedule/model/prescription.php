44447d2f6ea4220ae241bb0269f4e8d29bb78957
leo <leo@bm-sms.com.tw>
2014-06-04 16:34:02 +0800

    added API "/api/prescription/:id"

--- components/com_schedule/model/prescription.php
+++ components/com_schedule/model/prescription.php
@@ -9,6 +9,8 @@
 // No direct access
 defined('_JEXEC') or die;
 
+use Schedule\Table\Table;
+
 /**
  * Class ScheduleModelPrescription
  *
@@ -57,4 +59,71 @@ class ScheduleModelPrescription extends \Windwalker\Model\AdminModel
 	 * @var  string
 	 */
 	protected $viewList = 'prescriptions';
+
+	/**
+	 * Method to get a single record.
+	 *
+	 * @param   integer  $pk  The id of the primary key.
+	 *
+	 * @return  mixed    Object on success, false on failure.
+	 */
+	public function getItem($pk = null)
+	{
+		$item = parent::getItem($pk);
+
+		if (empty($item->id))
+		{
+			return $item;
+		}
+
+		$item->schedules = $this->getSchedules($item->id);
+		$item->drugs = $this->getDrugs($item->id);
+
+		return $item;
+	}
+
+	/**
+	 * getSchedules
+	 *
+	 * @param   int  $rxId  Prescription id
+	 *
+	 * @return  array
+	 */
+	public function getSchedules($rxId)
+	{
+		$db    = \JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('schedule.*')
+			->from(Table::SCHEDULES . ' AS schedule')
+			->where('`schedule`.`rx_id`=' . (int) $rxId);
+
+		$schedules = $db->setQuery($query)->loadObjectList();
+
+		foreach ($schedules as $schedule)
+		{
+			$schedule->params = (array) json_decode($schedule->params);
+		}
+
+		return $schedules;
+	}
+
+	/**
+	 * getDrugs
+	 *
+	 * @param   int  $rxId  Prescription id
+	 *
+	 * @return  array
+	 */
+	public function getDrugs($rxId)
+	{
+		$db    = \JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('drug.*')
+			->from(Table::DRUGS . ' AS drug')
+			->where('`drug`.`rx_id`=' . (int) $rxId);
+
+		return $db->setQuery($query)->loadObjectList();
+	}
 }
---
+++ components/com_schedule/view/prescription/json.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\View\ApiView;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleViewMemberJson
+ *
+ * @since 1.0
+ */
+class ScheduleViewPrescriptionJson extends ApiView
+{
+	/**
+	 * Property prefix.
+	 *
+	 * @var  string
+	 */
+	protected $prefix = 'schedule';
+
+	/**
+	 * Property option.
+	 *
+	 * @var  string
+	 */
+	protected $option = 'com_schedule';
+
+	/**
+	 * Property textPrefix.
+	 *
+	 * @var string
+	 */
+	protected $textPrefix = 'COM_SCHEDULE';
+
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'prescription';
+
+	/**
+	 * Property viewItem.
+	 *
+	 * @var  string
+	 */
+	protected $viewItem = 'prescription';
+
+	/**
+	 * Property viewList.
+	 *
+	 * @var  string
+	 */
+	protected $viewList = 'prescriptions';
+
+	/**
+	 * prepareData
+	 *
+	 * @return  void
+	 */
+	protected function prepareData()
+	{
+		$data = $this->getData();
+
+		$data['item'] = $this->get('Item');
+	}
+}
