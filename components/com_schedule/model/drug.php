874de10955cfbda277377519f9f016ec4ed2e6f3
leo <leo@bm-sms.com.tw>
2014-06-06 16:24:42 +0800

    added front-end models "Drug" and "Schedule"

---
+++ components/com_schedule/model/drug.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\Model\DrugModel;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelDrug
+ *
+ * @since 1.0
+ */
+class ScheduleModelDrug extends DrugModel
+{
+}
---
+++ components/com_schedule/model/schedule.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of Component Schedule files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Schedule\Model\ScheduleModel;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class ScheduleModelSchedule
+ *
+ * @since 1.0
+ */
+class ScheduleModelSchedule extends ScheduleModel
+{
+}
