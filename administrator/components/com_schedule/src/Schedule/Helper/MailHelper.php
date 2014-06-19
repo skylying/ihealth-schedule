8233ff3afb8cb66cb1d629edc33170aedfc56a2c
Lewis Jang <Lewis.AEdes@gmail.com>
2014-06-19 15:58:45 +0800

    add send mail

--- administrator/components/com_schedule/src/Schedule/Helper/MailHelper.php
+++ administrator/components/com_schedule/src/Schedule/Helper/MailHelper.php
@@ -73,4 +73,68 @@ class MailHelper
 	 * ```
 	 */
 	public $cc = array();
+
+	/**
+	 * Send Mail
+	 *
+	 * @return  MailHelper
+	 *
+	 * @throws  \Exception
+	 */
+	public function sendMail()
+	{
+		$mailer = \JFactory::getMailer();
+
+		if (empty($this->subject))
+		{
+			throw new \Exception("Please input some subject.");
+		}
+
+		if (empty($this->body))
+		{
+			throw new \Exception("Please input some content.");
+		}
+
+		if (empty($this->charset))
+		{
+			throw new \Exception("Please input content char encoding");
+		}
+
+		$mailer->Encoding = $this->charset;
+
+		$mailer->setSubject($this->subject);
+		$mailer->setBody($this->body);
+		$mailer->setSender($this->from);
+
+		foreach ($this->cc as $mailCCAddress)
+		{
+			$mailer->addCC($mailCCAddress);
+		}
+
+		foreach ($this->to as $mailToAddress)
+		{
+			$mailer->addRecipient($mailToAddress);
+		}
+
+		switch ($this->contentType)
+		{
+			case "text/html":
+				$mailer->isHtml(true);
+			break;
+			case "text/plain":
+				$mailer->isHtml(false);
+			break;
+			default:
+				$mailer->isHtml(true);
+		}
+
+		$sendMailDone = $mailer->Send();
+
+		if (! $sendMailDone)
+		{
+			throw new \Exception("Email send failure");
+		}
+
+		return $this;
+	}
 }
