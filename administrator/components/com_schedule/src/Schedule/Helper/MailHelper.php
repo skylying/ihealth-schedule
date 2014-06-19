a657ba2209a8dd4bfb6d023a74be05d9f6b64a89
Lewis Jang <Lewis.AEdes@gmail.com>
2014-06-19 18:42:59 +0800

    Refactor mail helper

--- administrator/components/com_schedule/src/Schedule/Helper/MailHelper.php
+++ administrator/components/com_schedule/src/Schedule/Helper/MailHelper.php
@@ -13,13 +13,6 @@ use Windwalker\View\Layout\FileLayout;
 class MailHelper
 {
 	/**
-	 * Property body.
-	 *
-	 * @var  string
-	 */
-	public $body = "";
-
-	/**
 	 * Property contentType.
 	 *
 	 * @var  string
@@ -34,13 +27,6 @@ class MailHelper
 	public $charset = "utf-8";
 
 	/**
-	 * Property subject.
-	 *
-	 * @var  string
-	 */
-	public $subject = "";
-
-	/**
 	 * Property from.
 	 *
 	 * @var  string
@@ -48,85 +34,35 @@ class MailHelper
 	public $from = 'ihealth@ihealth.com.tw';
 
 	/**
-	 * Property to.
+	 * sendMailWhenScheduleChange
 	 *
-	 * @var  array
-	 *
-	 * 變數設定參考
-	 * ```php
-	 * array(
-	 *     'a@user.mail',
-	 *     'b@user.mail',
-	 * )
-	 * ```
-	 */
-	public $to = array();
-
-	/**
-	 * Property cc.
-	 *
-	 * @var  array
-	 *
-	 * 變數設定參考
-	 * ```php
-	 * array(
-	 *     'a@user.mail',
-	 *     'b@user.mail',
-	 * )
-	 * ```
-	 */
-	public $cc = array();
-
-	/**
-	 * Send Mail
+	 * @param   string      $mailTo
+	 * @param   stdClass    $displayData
 	 *
 	 * @return  MailHelper
 	 *
-	 * @throws  \Exception
+	 * @throws \Exception
 	 */
-	public function sendMail()
+	public function sendMailWhenScheduleChange($mailTo, $displayData)
 	{
 		$mailer = \JFactory::getMailer();
 
-		if (empty($this->subject))
-		{
-			throw new \Exception("Please input some subject.");
-		}
-
-		if (empty($this->body))
-		{
-			throw new \Exception("Please input some content.");
-		}
-
-		if (empty($this->charset))
-		{
-			throw new \Exception("Please input content char encoding");
-		}
-
-		$mailer->Encoding = $this->charset;
-
-		$mailer->setSubject($this->subject);
-		$mailer->setBody($this->body);
+		// TODO: 確認標題文字
+		$mailer->setSubject("處方預約確認信");
+		$mailer->setBody((new FileLayout("scedule.mail.confirm"))->render($displayData));
+		$mailer->addRecipient($mailTo);
 		$mailer->setSender($this->from);
 
-		foreach ($this->cc as $mailCCAddress)
-		{
-			$mailer->addCC($mailCCAddress);
-		}
-
-		foreach ($this->to as $mailToAddress)
-		{
-			$mailer->addRecipient($mailToAddress);
-		}
+		$mailer->Encoding = $this->charset;
 
 		switch ($this->contentType)
 		{
 			case "text/html":
 				$mailer->isHtml(true);
-			break;
+				break;
 			case "text/plain":
 				$mailer->isHtml(false);
-			break;
+				break;
 			default:
 				$mailer->isHtml(true);
 		}
@@ -137,40 +73,6 @@ class MailHelper
 		{
 			throw new \Exception("Email send failure");
 		}
-
-		return $this;
-	}
-
-	/**
-	 * Send Mail When Schedule Change
-	 *
-	 * @param   \JUser      $user      Joomla user object
-	 * @param   Data[]      $schedules Schedules data
-	 * @param   Data        $rx        Rx data
-	 * @param   bool        $ccBack    When param is true, it will send mail to ihealth.
-	 *
-	 * @return  MailHelper
-	 */
-	public function sendMailWhenScheduleChange($user, $schedules, $rx, $ccBack = false)
-	{
-		$mailDisplayData = (object) array(
-			'user'     => $user,
-			'schedule' => $schedules,
-			'rx'       => $rx
-		);
-
-		// TODO: 確認標題
-		$this->subject = "處方預約確認信";
-		$this->body    = (new FileLayout("scedule.mail.default"))->render($mailDisplayData);
-		$this->to[]    = $user->email;
-
-		if ($ccBack)
-		{
-			// TODO: 確認給 ihealth 信箱的 mail address
-			$this->cc[] = "ihealth@ihealth.com.tw";
-		}
-
-		$this->sendMail();
 
 		return $this;
 	}
