5c9ec7e23b4737d482eff2f53bce7055b4b40690
leo <leo@bm-sms.com.tw>
2014-06-20 18:25:50 +0800

    use "MemberCustomerHelper" to get member data

--- administrator/components/com_schedule/controller/members/ajax/json.php
+++ administrator/components/com_schedule/controller/members/ajax/json.php
@@ -7,7 +7,7 @@
  */
 
 use Windwalker\Controller\DisplayController;
-use Schedule\Table\Table;
+use Schedule\Helper\Mapping\MemberCustomerHelper;
 
 /**
  * Class ScheduleControllerMembersAjaxJson
@@ -25,15 +25,9 @@ class ScheduleControllerMembersAjaxJson extends DisplayController
 	{
 		$id = (int) $this->input->get('id');
 
-		$db    = JFactory::getDbo();
-		$query = $db->getQuery(true);
+		$members = MemberCustomerHelper::loadMembers($id);
 
-		$query->select('`member`.`id`, `member`.`name`')
-			->from(Table::MEMBERS . ' AS `member`')
-			->leftJoin(Table::CUSTOMER_MEMBER_MAPS . ' AS `map` ON `map`.`member_id`=`member`.`id`')
-			->where("`map`.`customer_id`={$id}");
-
-		$response = json_encode($db->setQuery($query)->loadObjectList());
+		$response = json_encode($members);
 
 		JFactory::getDocument()->setMimeEncoding('application/json');
 
