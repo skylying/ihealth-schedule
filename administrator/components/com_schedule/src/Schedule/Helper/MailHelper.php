58b124ddc0cbce971b4705dbed243d04f34837c7
Lewis Jang <Lewis.AEdes@gmail.com>
2014-06-20 16:05:25 +0800

    Quality fix.

--- administrator/components/com_schedule/src/Schedule/Helper/MailHelper.php
+++ administrator/components/com_schedule/src/Schedule/Helper/MailHelper.php
@@ -6,26 +6,12 @@ use Windwalker\Data\Data;
 use Windwalker\View\Layout\FileLayout;
 
 /**
- * Class ScheduleHelper
+ * Class MailHelper
  *
  * @since 1.0
  */
 class MailHelper
 {
-	/**
-	 * Property contentType.
-	 *
-	 * @var  string
-	 */
-	public static $contentType = "text/html";
-
-	/**
-	 * Property charset.
-	 *
-	 * @var  string
-	 */
-	public static $charset = "utf-8";
-
 	/**
 	 * Property from.
 	 *
@@ -45,7 +31,20 @@ class MailHelper
 	 */
 	public static function sendMailWhenScheduleChange($mailTo, $displayData)
 	{
-		static::sendMailProcessor($mailTo, "處方預約確認信", "schedule.mail.confirm", $displayData);
+		$mailer = \JFactory::getMailer();
+
+		$mailer->setSubject("處方預約確認信");
+		$mailer->setBody((new FileLayout("schedule.mail.confirm"))->render($displayData));
+		$mailer->addRecipient($mailTo);
+		$mailer->setSender(static::$from);
+		$mailer->isHtml(true);
+
+		$sendMailDone = $mailer->Send();
+
+		if (! $sendMailDone)
+		{
+			throw new \Exception("Email send failure");
+		}
 	}
 
 	/**
@@ -60,32 +59,13 @@ class MailHelper
 	 */
 	public static function sendEmptyRouteMail($mailTo, $displayData)
 	{
-		static::sendMailProcessor($mailTo, "沒有路線通知", "schedule.mail.emptyroute", $displayData);
-	}
-
-	/**
-	 * Send Mail Processor
-	 *
-	 * @param   string  $mailTo
-	 * @param   string  $subject
-	 * @param   string  $layout
-	 * @param   object  $displayData
-	 *
-	 * @return  void
-	 *
-	 * @throws \Exception
-	 */
-	public static function sendMailProcessor($mailTo, $subject, $layout, $displayData)
-	{
 		$mailer = \JFactory::getMailer();
 
-		$mailer->setSubject($subject);
-		$mailer->setBody((new FileLayout($layout))->render($displayData));
+		$mailer->setSubject("沒有路線通知");
+		$mailer->setBody((new FileLayout("schedule.mail.emptyroute"))->render($displayData));
 		$mailer->addRecipient($mailTo);
 		$mailer->setSender(static::$from);
-		$mailer->isHtml("text/plain" !== static::$contentType);
-
-		$mailer->CharSet = static::$charset;
+		$mailer->isHtml(true);
 
 		$sendMailDone = $mailer->Send();
 
