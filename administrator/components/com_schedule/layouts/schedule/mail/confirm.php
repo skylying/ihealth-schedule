3a2c634388ba3eff4d93bfba1d73ecd976d5dfc9
takeshiwayne <wayne@bm-sms.com.tw>
2014-06-23 09:45:26 +0800

    把塞資料的地方標注出來

--- administrator/components/com_schedule/layouts/schedule/mail/confirm.php
+++ administrator/components/com_schedule/layouts/schedule/mail/confirm.php
@@ -18,8 +18,8 @@
 		以下是您的預約宅配資料
 	</title>
 </head>
-<div class="container">
 
+<div class="container">
 	<div class="row">
 		<div>
 			<img src="<?php echo JUri::root(true) . '/media/media/images/ihealth.png' ?>" class="img-responsive" alt="Responsive image" />
@@ -27,32 +27,32 @@
 	</div>
 
 	<div class="row">
-		<h2 class="text-center">先生/小姐 您好：以下是您的預約宅配資料</h2>
+		<h2 class="text-center"><span><!--Customer Name--></span>先生/小姐 您好：以下是您的預約宅配資料</h2>
 		<h4 class="bg-success">小叮嚀：外送藥師拜訪時, 請準備好您的健保卡</h4>
 		<table class="table table-striped">
 			<tr>
 				<td>宅配編號</td>
-				<td></td>
+				<td><!--Schedule id--></td>
 			</tr>
 			<tr>
 				<td>處方姓名</td>
-				<td></td>
+				<td><!--Customer name--></td>
 			</tr>
 			<tr>
 				<td>身分證字號</td>
-				<td></td>
+				<td><!--Id number--></td>
 			</tr>
 			<tr>
 				<td>藥師送藥日期</td>
-				<td></td>
+				<td><!--Date--></td>
 			</tr>
 			<tr>
 				<td>藥師送藥時段</td>
-				<td></td>
+				<td><!--Session--><</td>
 			</tr>
 			<tr>
 				<td>藥師送藥地址</td>
-				<td></td>
+				<td><!--Full address--></td>
 			</tr>
 		</table>
 	</div>
@@ -63,27 +63,27 @@
 		<table class="table table-striped">
 			<tr>
 				<td>就醫日期</td>
-				<td></td>
+				<td><!--see dr date--></td>
 				<td>處方箋傳送方式</td>
-				<td></td>
+				<td><!--method--></td>
 			</tr>
 			<tr>
 				<td>可調劑次數</td>
-				<td></td>
+				<td><!--Times--></td>
 				<td>醫師姓名</td>
-				<td></td>
+				<td><!--醫師姓名--></td>
 			</tr>
 			<tr>
 				<td>第幾次領藥</td>
-				<td></td>
+				<td><!--deliver nth--></td>
 				<td>國際疾病代碼</td>
-				<td></td>
+				<td><!--國際疾病代碼--></td>
 			</tr>
 			<tr>
 				<td>給藥天數</td>
-				<td></td>
+				<td><!--period--></td>
 				<td>藥品健保碼</td>
-				<td></td>
+				<td><!--hicode--></td>
 			</tr>
 		</table>
 	</div>
@@ -99,8 +99,6 @@
 			<h3>藥師親自宅配. 最方便. 最放心.</h3>
 		</div>
 	</div>
-
 	<p>SMS Taiwan Copyright (C) SMS Consulting Service Co.,Ltd.</p>
-
 </div>
 </html>
