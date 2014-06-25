44ad0f1b8121cc430abca60813f091f6450b19e7
leo <leo@bm-sms.com.tw>
2014-06-25 18:40:06 +0800

    refactor institutes search api

--- administrator/components/com_schedule/controller/institutes/search/json.php
+++ administrator/components/com_schedule/controller/institutes/search/json.php
@@ -8,91 +8,76 @@
 
 use Windwalker\Controller\DisplayController;
 
+/**
+ * Class ScheduleControllerInstitutesSearchJson
+ *
+ * @since 1.0
+ */
 class ScheduleControllerInstitutesSearchJson extends DisplayController
 {
 	/**
 	 * This will create json response and return it
 	 *
-	 *  - Api format :
+	 * ## Api format
 	 *   - base url : index.php?option=com_schedule
 	 *   - api keys :
 	 *     - task = institutes.search.json
 	 *     - filter_search = {institute name}
 	 *
-	 *   EX : index.php?option=com_schedule&task=institutes.search.json&filter_search=新北
+	 * EX : index.php?option=com_schedule&task=institutes.search.json&filter_search=新北
 	 *
-	 * - Returned data : (JSON FORMAT)
-	 *  - "id"           = institute id + floor title (for select2 use)
-	 *  - "instituteid   = institute id
-	 *  - "dropdowntext" = institute shorttitle
-	 *  - "color"        = color hex code
-	 *  - "delivery_day" = delivery weekday
-	 *  - "floor"        = floor title
-	 *  - "city"         = city id
-	 *  - "area"         = area id
+	 * ## Returned data (JSON FORMAT)
+	 *   - "id"               = institute id + floor title (for select2 use, separated by a delimiter '-')
+	 *   - "short_title"      = institute short title
+	 *   - "color_hex"        = color hex code
+	 *   - "delivery_weekday" = delivery weekday
+	 *   - "floor"            = floor title
+	 *   - "city"             = city id
+	 *   - "area"             = area id
 	 *
-	 * @return  mixed|void
+	 * @return  void
 	 */
 	protected function doExecute()
 	{
 		$queryString = JFactory::getApplication()->input->getString('filter_search');
 
+		if (empty($queryString))
+		{
+			jexit('[]');
+		}
+
 		$model = $this->getModel('Institutes');
 
-		$state = $model->getState();
+		$model->getState()->set('search', array('institute.short_title' => $queryString));
 
-		$state->set('search', array('institute.short_title' => $queryString));
+		$items = array();
 
-		$items = array_map(
-			function ($item)
-			{
-				return array(
-					'id'           => $item->id,
-					'dropdowntext' => $item->institute_short_title,
-					'color'        => $item->color_hex,
-					'delivery_day' => $item->delivery_weekday,
-					'floor'        => $item->floor,
-					'city'         => $item->city,
-					'area'         => $item->area
-				);
-			},
-			$model->getItems()
-		);
-
-		// Prepare floor data
-		$itemsWithFloors = array();
-
-		foreach ($items as $item)
+		foreach ($model->getItems() as $item)
 		{
-			$floorArray = explode(',', $item['floor']);
+			$data = array(
+				'color_hex'        => $item->color_hex,
+				'delivery_weekday' => $item->delivery_weekday,
+				'floor'            => $item->floor,
+				'city'             => $item->city,
+				'area'             => $item->area,
+			);
 
-			foreach ($floorArray as $value)
+			foreach (explode(',', $item->floor) as $floor)
 			{
-				$itemsWithFloors[] = array(
+				/*
+				 * select2 在 onchange 時會把回傳的 "id" 塞進本身的 value 裏面
+				 * 但這邊因應同一間機構在 id 相同狀況下必須更新樓層欄位的需求
+				 * 必須把 ajax 回傳的 "id" 做出區隔 (在這邊是後綴上 $value)
+				 * 才能滿足 select2 "onchange" 的事件觸發, 否則同一間機構,
+				 * 不同樓層, 再怎麼選 id 都一樣, 無法觸發 onchange 事件
+				 */
+				$data['id'] = $item->id . '-' . $floor;
+				$data['short_title'] = $item->institute_short_title . ' ' . $floor;
 
-					/*
-					 * select2 在 onchange 時會把回傳的 "id" 塞進本身的 value 裏面
-					 * 但這邊因應同一間機構在 id 相同狀況下必須更新樓層欄位的需求
-					 * 必須把 ajax 回傳的 "id" 做出區隔 (在這邊是後綴上 $value)
-					 * 才能滿足 select2 "onchange" 的事件觸發, 否則同一間機構,
-					 * 不同樓層, 再怎麼選 id 都一樣, 無法觸發 onchange 事件
-					 */
-					'id' => $item['id'] . $value,
-
-					/*
-					 * 在 select2 的 onchange callback 中再把這個真正的 id 塞回 value
-					 */
-					'instituteid'  => $item['id'],
-					'dropdowntext' => $item['dropdowntext'] . ' ' . $value,
-					'color'        => $item['color'],
-					'delivery_day' => $item['delivery_day'],
-					'floor'        => $value,
-					'city'         => $item['city'],
-					'area'         => $item['area']
-				);
+				$items[] = $data;
 			}
 		}
 
-		jexit(json_encode($itemsWithFloors));
+		jexit(json_encode($items));
 	}
 }
