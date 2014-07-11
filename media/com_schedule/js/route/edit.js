757d12c1a7937614c32946a4f345672ca58bc8d3
leo <leo@bm-sms.com.tw>
2014-07-11 16:21:29 +0800

    updated layout in route edit page

--- media/com_schedule/js/route/edit.js
+++ media/com_schedule/js/route/edit.js
@@ -29,12 +29,17 @@
 			this.$typeRadios = this.$typeField.find('input');
 			this.$instituteControl  = $('#control_jform_institute_id');
 			this.$instituteSelector = $('#jform_institute_id');
+			this.$city = $('#jform_city');
+			this.$area = $('#jform_area');
 
 			// Register events
 			this.registerEvents();
 
-			// init selector
-			this.toggleInstituteSelector(this.$typeField.find('input:checked').val());
+			// Hide institute_id selection when route type is customer
+			if ('institute' !== this.$typeField.find('input:checked').val())
+			{
+				this.$instituteControl.hide();
+			}
 
 			// Disable some fields in edit page
 			if (options.isEdit)
@@ -72,12 +77,57 @@
 			if ('institute' === condition)
 			{
 				this.$instituteControl.show();
+
+				this.$city.attr('disabled', true);
+				this.$area.attr('disabled', true);
 			}
 			else
 			{
 				this.$instituteControl.hide();
 
 				this.$instituteSelector.select2('val', '');
+
+				this.$city.attr('disabled', false);
+				this.$area.attr('disabled', false);
+			}
+
+			this.$city.val('');
+			this.$city.change();
+			this.$city.trigger('liszt:updated');
+
+			this.$area.val('');
+			this.$area.trigger('liszt:updated');
+		},
+
+		/**
+		 * Triggered by institute_id onchange event, will update "縣市", "區域"
+		 *
+		 * It will return a callback function, which contains params:
+		 * - {object}  e     Event object, Contains the following custom properties:
+		 *                        - val:     The current selection (taking into account the result of the change) - id or array of ids.
+		 *                        - added:   The added element, if any - the full element object, not just the id.
+		 *                        - removed: The removed element, if any - the full element object, not just the id.
+		 * - {element} $node Current Element node
+		 */
+		instituteIdChange: function()
+		{
+			var self = this;
+
+			return function(e, $node)
+			{
+				var data = $node.select2('data');
+
+				if (!data)
+				{
+					return;
+				}
+
+				self.$city.val(data.city);
+				self.$city.change();
+				self.$city.trigger('liszt:updated');
+
+				self.$area.val(data.area);
+				self.$area.trigger('liszt:updated');
 			}
 		}
 	};
