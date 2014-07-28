dbd58b9e94acd5e546e41c0649853305c35138be
skylying <linsutim@gmail.com>
2014-07-28 16:38:17 +0800

    change schedule print view name

--- administrator/components/com_schedule/view/schedules/tmpl/default.php
+++ administrator/components/com_schedule/view/schedules/tmpl/default.php
@@ -244,7 +244,7 @@ $editFormFields = $data->editFormFields;
 <div id="modal-report-print" class="modal hide fade" tabindex="-1" role="dialog">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		<h3 class="modal-title">列印排程統計報表</h3>
+		<h3 class="modal-title">處方統計表</h3>
 	</div>
 	<div class="modal-body row-fluid">
 		<?php
