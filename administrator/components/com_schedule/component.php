e69b674d52869e17b32ad116b7eeb49c556ba3d6
skylying <linsutim@gmail.com>
2014-08-01 11:22:26 +0800

    move redirect rule to prepare method in component.php

--- administrator/components/com_schedule/component.php
+++ administrator/components/com_schedule/component.php
@@ -7,6 +7,7 @@
  */
 
 use Schedule\Component\ScheduleComponent as ScheduleComponentBase;
+use Schedule\Helper\SenderHelper;
 
 // No direct access
 defined('_JEXEC') or die;
@@ -32,6 +33,15 @@ final class ScheduleComponent extends ScheduleComponentBase
 	 */
 	public function prepare()
 	{
+		// If user belongs to sender, redirect to view = tasks
+		$sender = SenderHelper::checkSender();
+		$app = JFactory::getApplication();
+
+		if ($sender && $app->input->get('view') !== 'tasks')
+		{
+			$app->redirect(JUri::root() . '/administrator/index.php?option=com_schedule&view=tasks');
+		}
+
 		parent::prepare();
 
 		$asset = $this->container->get('helper.asset');
