cfa7f2da6d92353bab4cc53593dc1fbbb2866f7a
bblurock <llwbenson@gmail.com>
2014-08-04 15:38:23 +0800

    調整多筆排程時，可允許其中一個欄位(sender 或是 date)為空。僅集體修改被有帶入的欄位。

--- administrator/components/com_schedule/controller/schedules/edit.php
+++ administrator/components/com_schedule/controller/schedules/edit.php
@@ -72,44 +72,48 @@ class ScheduleControllerSchedulesEdit extends AbstractUpdateStateController
 		parent::prepareExecute();
 
 		$taskMapper = new DataMapper(Table::TASKS);
+		$scheduleMapper = new DataMapper(Table::SCHEDULES);
 
 		$this->validData = $this->validate();
-
-		$this->stateData['date']        = $this->validData['date'];
-		$this->stateData['sender_name'] = $this->validData['sender_name'];
-
-		// Get task data
-		$task = $taskMapper->findOne(
-			[
-				'date' => $this->validData['date'],
-				'sender' => $this->validData['sender_id'],
-			]
-		);
-
-		// If task data is not found, create a new task
-		if ($task->isNull())
-		{
-			$taskModel = $this->getModel('Task');
-			$task = [
-				'date' => $this->validData['date'],
-				'sender' => $this->validData['sender_id'],
-				'sender_name' => $this->validData['sender_name'],
-				'status' => 0,
-			];
-
-			$taskModel->save($task);
-
-			$task['id'] = $taskModel->getState()->get('task.id');
-
-			$task = new Data($task);
-		}
-
-		$this->stateData['task_id'] = $task->id;
 
 		foreach ($this->input->get('cid', array(), 'ARRAY') as $id)
 		{
 			if ($id > 0)
 			{
+				$schedule = $scheduleMapper->findOne(['id' => $id]);
+
+				$this->stateData['date']        = !empty($this->validData['date']) ? $this->validData['date'] : $schedule->date;
+				$this->stateData['sender_name'] = !empty($this->validData['sender_name']) ? $this->validData['sender_name'] : $schedule->sender_name;
+				$this->stateData['sender_id']   = !empty($this->validData['sender_id']) ? $this->validData['sender_id'] : $schedule->sender_id;
+
+				// Get task data
+				$task = $taskMapper->findOne(
+					[
+						'date' => $this->stateData['date'],
+						'sender' => $this->stateData['sender_id'],
+					]
+				);
+
+				// If task data is not found, create a new task
+				if ($task->isNull())
+				{
+					$taskModel = $this->getModel('Task');
+					$task = [
+						'date' => $this->stateData['date'],
+						'sender' => $this->stateData['sender_id'],
+						'sender_name' => $this->stateData['sender_name'],
+						'status' => 0,
+					];
+
+					$taskModel->save($task);
+
+					$task['id'] = $taskModel->getState()->get('task.id');
+
+					$task = new Data($task);
+				}
+
+				$this->stateData['task_id'] = $task->id;
+
 				$oldScheduleTable = TableCollection::loadTable('Schedule', $id);
 
 				if (! empty($oldScheduleTable->id)
@@ -174,13 +178,31 @@ class ScheduleControllerSchedulesEdit extends AbstractUpdateStateController
 		{
 			throw new \InvalidArgumentException('請至少選擇一個排程');
 		}
-
-		// Validate date
-		$testDate = explode('-', $date);
-
-		if (count($testDate) !== 3 || !checkdate($testDate[1], $testDate[2], $testDate[0]))
+		else
 		{
-			throw new \InvalidArgumentException('日期輸入錯誤 "' . $date . '"');
+			if (empty($date) && empty($senderId))
+			{
+				throw new \InvalidArgumentException('請至少選擇一種選項修改');
+			}
+		}
+
+		if (!empty($date))
+		{
+			// Validate date
+			$testDate = explode('-', $date);
+
+			if (count($testDate) !== 3 || !checkdate($testDate[1], $testDate[2], $testDate[0]))
+			{
+				throw new \InvalidArgumentException('日期輸入錯誤 "' . $date . '"');
+			}
+		}
+
+		if (empty($senderId))
+		{
+			// If date is valid, senderId does not exist. Forget about sender_id, and return validated data.
+			return array(
+				'date' => $date,
+			);
 		}
 
 		// Validate sender id
