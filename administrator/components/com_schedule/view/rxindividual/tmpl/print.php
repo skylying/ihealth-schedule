9ddf7561a291396dfa261dab86f8a79c4b116fdd
wayne <wayne@bm-sms.com.tw>
2014-08-08 18:48:27 +0800

    修整print 頁面。

--- administrator/components/com_schedule/view/rxindividual/tmpl/print.php
+++ administrator/components/com_schedule/view/rxindividual/tmpl/print.php
@@ -31,7 +31,7 @@ function markPrinted()
 </script>
 
 <div class="row-fluid">
-	<div class="col-lg-12 center printButton">
+	<div class="col-lg-12 center printButton" style="padding-top: 10px;">
 		<a class="btn btn-default btn-info" onclick="window.print();markPrinted();">
 			<i class="glyphicon glyphicon-print">
 				列印
@@ -45,7 +45,7 @@ function markPrinted()
 	</div>
 </div>
 <?php foreach ($scheduleInfos as $scheduleInfo): ?>
-	<div class="row-fluid">
+	<div class="row-fluid" style="margin-top: 20px;">
 		<div class="col-lg-12">
 			<h2 class="center">
 				<?php echo $data->item->customer_name; ?>
@@ -65,7 +65,7 @@ function markPrinted()
 	</div>
 	<div class="row-fluid">
 		<div class="col-lg-12 left">
-			<table class="table table-striped">
+			<table class="table table-striped" style="border: 1px solid #C0BCBC;">
 				<thead>
 				<tr>
 					<th class="left">項次</th>
@@ -78,79 +78,79 @@ function markPrinted()
 				<tr>
 					<td>所屬會員</td>
 					<td><?php echo $data->item->member_name; ?></td>
-					<td>宅配次數</td>
-					<td>第 <?php echo substr($scheduleInfo->deliver_nth, 0, 1); ?> 次</td>
-				</tr>
-				<tr>
-					<td>處方姓名</td>
-					<td><?php echo $data->item->customer_name; ?></td>
-					<td>藥品吃完日</td>
-					<td><?php echo $scheduleInfo->drug_empty_date; ?></td>
-				</tr>
-				<tr>
 					<td>客戶身分證字號</td>
 					<td><?php echo $data->item->id_number; ?></td>
-					<td>預約宅配日</td>
-					<td><?php echo $scheduleInfo->date; ?></td>
 				</tr>
 				<tr>
+					<td style="font-weight: bold; font-size: 16px;">處方姓名</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo $data->item->customer_name; ?></td>
 					<td>客戶生日</td>
 					<td><?php echo $data->item->birth_date; ?></td>
-					<td>時段</td>
-					<td><?php echo JText::_('COM_SCHEDULE_RXINDIVIDUAL_PRINT_' . $scheduleInfo->session); ?></td>
 				</tr>
 				<tr>
-					<td>客戶年齡</td>
-					<td><?php echo $data->item->age; ?></td>
-					<td>地址</td>
-					<td><?php echo $scheduleInfo->city_title . ' - ' . $scheduleInfo->area_title . ' - ' . $scheduleInfo->address; ?></td>
+					<td style="font-weight: bold; font-size: 16px;">客戶備註</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo nl2br($data->item->customer_note); ?></td>
+					<td>處方開立醫院</td>
+					<td><?php echo $data->item->hospital_title; ?></td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold; font-size: 16px;">本次外送備註</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo nl2br($data->item->note); ?></td>
+					<td><!--科別--></td>
+					<td><!--科別--></td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold; font-size: 16px;">地址</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo $scheduleInfo->city_title . ' - ' . $scheduleInfo->area_title . ' - ' . $scheduleInfo->address; ?></td>
+					<td>就醫日期</td>
+					<td><?php echo $data->item->see_dr_date; ?></td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold; font-size: 16px;">電話(H)</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo $scheduleInfo->tel_home; ?></td>
+					<td>給藥天數</td>
+					<td><?php echo $data->item->period; ?>天</td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold; font-size: 16px;">電話(O)</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo $scheduleInfo->tel_office; ?></td>
+					<td>可調劑次數</td>
+					<td><?php echo $data->item->times; ?>次</td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold; font-size: 16px;">手機(M)</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo $scheduleInfo->mobile; ?></td>
+					<td><!--是否分包--></td>
+					<td><?php // echo JText::_('COM_SCHEDULE_RXINDIVIDUAL_PRINT_NEEDSPLIT_' . $data->item->need_split);?></td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold; font-size: 16px;">預約宅配日</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo $scheduleInfo->date; ?></td>
+					<td><!--外送次數--></td>
+					<td><?php // echo $data->item->times; ?></td>
+				</tr>
+				<tr>
+					<td style="font-weight: bold; font-size: 16px;">時段</td>
+					<td style="font-weight: bold; font-size: 16px;"><?php echo JText::_('COM_SCHEDULE_RXINDIVIDUAL_PRINT_' . $scheduleInfo->session); ?></td>
+					<td><!--客戶年齡--></td>
+					<td><?php // echo $data->item->age; ?></td>
 				</tr>
 				<tr>
 					<td>處方箋狀態</td>
 					<td><?php echo $data->item->received ? '取得' : '未取得'; ?></td>
-					<td>電話(H)</td>
-					<td><?php echo $scheduleInfo->tel_home; ?></td>
+					<td><!--藥品吃完日--></td>
+					<td><?php // echo $scheduleInfo->drug_empty_date; ?></td>
 				</tr>
 				<tr>
 					<td>處方箋上傳方式</td>
 					<td><?php echo JText::_('COM_SCHEDULE_RXINDIVIDUAL_PRINT_' . $data->item->method); ?></td>
-					<td>電話(O)</td>
-					<td><?php echo $scheduleInfo->tel_office; ?></td>
-				</tr>
-				<tr>
-					<td>處方開立醫院</td>
-					<td><?php echo $data->item->hospital_title; ?></td>
-					<td>手機(M)</td>
-					<td><?php echo $scheduleInfo->mobile; ?></td>
-				</tr>
-				<tr>
-					<td>就醫日期</td>
-					<td><?php echo $data->item->see_dr_date; ?></td>
-					<td>本次外送備註</td>
-					<td><?php echo nl2br($data->item->note); ?></td>
-				</tr>
-				<tr>
-					<td>給藥天數</td>
-					<td><?php echo $data->item->period; ?>天</td>
-					<td>客戶備註</td>
-					<td><?php echo nl2br($data->item->customer_note); ?></td>
-				</tr>
-				<tr>
-					<td>可調劑次數</td>
-					<td><?php echo $data->item->times; ?>次</td>
-					<td>是否分包</td>
-					<td>
-						<?php echo JText::_('COM_SCHEDULE_RXINDIVIDUAL_PRINT_NEEDSPLIT_' . $data->item->need_split);?>
-					</td>
-				</tr>
-				<tr>
-					<td>外送次數</td>
-					<td><?php echo $data->item->times; ?></td>
-					<td></td>
-					<td></td>
+					<td><!--宅配次數--></td>
+					<td><!--第 <?php // echo substr($scheduleInfo->deliver_nth, 0, 1); ?> 次--></td>
 				</tr>
 				<?php if ($data->item->method == 'form'): ?>
-				<tr>
+				<tr >
+					<td></td>
+					<td></td>
 					<td>藥品資料</td>
 					<td>
 						<table class="table table-bordered">
@@ -161,7 +161,7 @@ function markPrinted()
 							</tr>
 							</thead>
 							<tbody>
-							<?php foreach ($drugs as $drug): ?>
+							<?php foreach ($drugs as $drug):?>
 								<tr>
 									<td>
 										<?php echo $drug->hicode; ?>
@@ -177,10 +177,10 @@ function markPrinted()
 				</tr>
 				<?php endif; ?>
 				<tr>
+					<td></td>
+					<td></td>
 					<td>藥品種數</td>
 					<td><?php echo $data->item->count; ?></td>
-					<td></td>
-					<td></td>
 				</tr>
 				</tbody>
 			</table>
