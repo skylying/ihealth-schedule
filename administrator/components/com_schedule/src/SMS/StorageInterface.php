ff06af44fab619cffa46d41f1cccf74470368030
leo <leo@bm-sms.com.tw>
2014-08-28 22:48:25 +0800

    added class "StorageFactory"

---
+++ administrator/components/com_schedule/src/SMS/StorageInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SMS;
+
+/**
+ * Interface StorageInterface
+ */
+interface StorageInterface
+{
+	/**
+	 * Put file to storage
+	 *
+	 * @param   string  $localFilePath    Local file path
+	 * @param   string  $storageFilePath  Storage file path
+	 *
+	 * @return  mixed
+	 */
+	public function put($localFilePath, $storageFilePath);
+
+	/**
+	 * Delete storage file
+	 *
+	 * @param   string  $storageFilePath  Storage file path
+	 *
+	 * @return  mixed
+	 */
+	public function delete($storageFilePath);
+}
