3e51f4f01b9af650016571aaf7a6786ea90714a8
leo <leo@bm-sms.com.tw>
2014-09-16 18:31:16 +0800

    added task "customers.ajax.json"

---
+++ administrator/components/com_schedule/controller/customers/ajax/json.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Part of ihealth-schedule project.
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+use Windwalker\Controller\DisplayController;
+
+/**
+ * Class ScheduleControllerCustomersAjaxJson
+ *
+ * @since 1.0
+ */
+class ScheduleControllerCustomersAjaxJson extends DisplayController
+{
+	/**
+	 * doExecute
+	 *
+	 * @return  mixed|void
+	 */
+	protected function doExecute()
+	{
+		$query = $this->input->getString('q');
+
+		/** @var ScheduleModelCustomers $model */
+		$model = $this->getModel('Customers', '', array('ignore_request' => true));
+		$state = $model->getState();
+
+		$state->set(
+			'search',
+			array(
+				'customer.name' => $query,
+			)
+		);
+
+		$items = array();
+
+		foreach ($model->getItems() as $item)
+		{
+			$items[] = array(
+				'id' => $item->id,
+				'name' => $item->name,
+				'value' => $item->id,      // For AjaxChosen
+				'text' => $item->name,     // For AjaxChosen
+			);
+		}
+
+		if (count($items) > 0)
+		{
+			jexit(json_encode($items));
+		}
+
+		jexit('{}');
+	}
+}
