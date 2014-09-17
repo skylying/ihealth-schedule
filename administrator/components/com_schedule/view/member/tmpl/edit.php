2275751e525fcad8f6046f5666696bb6d290190f
skylying <linsutim@gmail.com>
2014-09-17 16:43:10 +0800

    render fake mail in template

--- administrator/components/com_schedule/view/member/tmpl/edit.php
+++ administrator/components/com_schedule/view/member/tmpl/edit.php
@@ -28,6 +28,22 @@ $infoFieldset = $fieldsets['information'];
 $relatedCustomersFieldset = $fieldsets['related_customers'];
 ?>
 
+<script>
+	(function($)
+	{
+		$(document).ready(function()
+		{
+			var generateMailBtn = $('#generate-mail-btn'),
+				fakeMailContainer = $('#fake-mail');
+
+			generateMailBtn.on('click', function()
+			{
+				fakeMailContainer.animate({"opacity":1}, 800);
+			})
+		})
+	})(jQuery)
+</script>
+
 <div id="schedule" class="windwalker member edit-form row-fluid">
 	<form action="<?php echo JURI::getInstance(); ?>"  method="post" name="adminForm" id="adminForm"
 		class="form-validate" enctype="multipart/form-data" autocomplete="off">
@@ -51,6 +67,19 @@ $relatedCustomersFieldset = $fieldsets['related_customers'];
 							</div>
 						</div>
 					<?php endforeach;?>
+
+					<?php if ($data->hasFakeMail): ?>
+					<div class="control-group">
+						<div class="control-label">
+							<label id="" for="generate-mail-btn" class="hasTooltip" title="" data-original-title="<strong>產生假信箱</strong>">產生假信箱</label>
+						</div>
+						<div class="controls">
+							<button id="generate-mail-btn" class="btn btn-info" type="button">點此產生</button>
+							<span id="fake-mail" class="alert alert-info" style="opacity:0;"><?php echo isset($data->fakeMail) ? $data->fakeMail : ''; ?></span>
+						</div>
+					</div>
+					<?php endif; ?>
+
 				</fieldset>
 			</div>
 			<div class="span6">
